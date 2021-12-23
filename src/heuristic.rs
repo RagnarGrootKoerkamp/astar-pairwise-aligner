12483045e7fc6fb982c9b0263f5813d382fcd632
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2021-12-23 02:44:20 +0100

    Rename fast{->_zero}_seed_heuristic.rs

--- src/heuristic.rs
+++ src/heuristic.rs
@@ -1,13 +1,13 @@
 pub mod distance;
 pub mod equal_heuristic;
-pub mod fast_seed_heuristic;
+pub mod fast_zero_seed_heuristic;
 pub mod heuristic;
 pub mod pathmax;
 pub mod seed_heuristic;
 
 pub use distance::*;
 pub use equal_heuristic::*;
-pub use fast_seed_heuristic::*;
+pub use fast_zero_seed_heuristic::*;
 pub use heuristic::*;
 pub use pathmax::*;
 pub use seed_heuristic::*;
