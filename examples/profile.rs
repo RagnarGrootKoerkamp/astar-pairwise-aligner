06d1553e3f67c4077490a169f19598a67d3b0069
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-01-08 19:29:53 +0100

    Latest examples

--- examples/profile.rs
+++ examples/profile.rs
@@ -1,32 +1,24 @@
 use pairwise_aligner::{prelude::*, *};
 
 fn main() {
-    let mut wtr = csv::WriterBuilder::new()
-        .has_headers(true)
-        .from_path("evals/stats/table.csv")
-        .unwrap();
-
-    let n = 300;
+    let n = 30000;
     let e = 0.10;
-    let l = 3;
+    let l = 5;
     let max_match_cost = 0;
     let pruning = true;
 
-    let result = {
-        let h = GapSeedHeuristic {
-            match_config: MatchConfig {
-                length: Fixed(l),
-                max_match_cost,
-                ..MatchConfig::default()
-            },
-            pruning,
-            prune_fraction: 0.5,
-            c: PhantomData::<NaiveContours<NaiveContour>>,
-            ..GapSeedHeuristic::default()
-        };
-        let (a, b, alphabet, stats) = setup(n, e);
-        align(&a, &b, &alphabet, stats, h)
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(l),
+            max_match_cost,
+            ..MatchConfig::default()
+        },
+        pruning,
+        prune_fraction: 0.5,
+        c: PhantomData::<NaiveContours<NaiveContour>>,
+        ..GapSeedHeuristic::default()
     };
+    let (a, b, alphabet, stats) = setup(n, e);
+    let result = align(&a, &b, &alphabet, stats, h);
     result.print();
-    result.write(&mut wtr);
 }
