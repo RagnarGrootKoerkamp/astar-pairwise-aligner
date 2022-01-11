94830cc87d3a41cedb9c49b251b0c9f5d1e18cd7
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-01-11 16:46:05 +0100

    Use shrink and fuzz for the first time

--- examples/shrink.rs
+++ examples/shrink.rs
@@ -1,11 +1,12 @@
 use pairwise_aligner::prelude::*;
 
 fn main() {
-    let n = 1000;
-    let e = 0.20;
-    let l = 7;
-    let max_match_cost = 1;
+    let n = 100;
+    let e = 0.30;
+    let l = 4;
+    let max_match_cost = 0;
     let pruning = true;
+    let prune_fraction = 1.0;
 
     let h = GapSeedHeuristic {
         match_config: MatchConfig {
@@ -14,65 +15,63 @@ fn main() {
             ..MatchConfig::default()
         },
         pruning,
-        prune_fraction: 0.5,
+        prune_fraction,
         c: PhantomData::<NaiveContours<LogQueryContour>>,
         ..GapSeedHeuristic::default()
-    };
-
-    let (_a, _b, alphabet, stats) = setup(n, e);
-    let a = "TTGCATGGAAAGCTTGCTTATCTGTTTTCTCCTTGTTCGTCGGGCGAGGATCCGAAGTAAGCCGCTGCAAAGTTACCCATCAATGATCATCTCGCTGTAGACCAACCGGCCAGGTATTCTTTGCCTATTCGTTTATCCGATAACGAGGTACGTAAAAAATGGTCCTCTCTTATTGGAAATCGGAGGCGTCGGACTGCCCGAAGAACGACTTTCGAACCTATCTTGGCTAGGATACTCATTTTGCCGGGACTTGGTCCCCCATGTAAGGAGGTCCCTATGACCAAATCGCTACTGAAGTGTCATTGAGGAAGCGTCGTTCATCCTAGTTCGGGTCGCGATGAGATGGCCGTGGATCACCTTTGGGTAACATACGCAATGCGCTACTGGCTGATAGCAGCGTCTATACTATCGCCTTACCGAAGTCCCCATATCTTAACAAACTCTGCAAGGCTACAGGGGGTCTTCATGAAGGAAGTCGTAAAACGATATCTGTGCATCTTGGGGGACAGCCTGCAAGACCGAACTGGTACTGTCCCGTGCAAATCCTTCCCTGCCGATCAAATTGACATAGAATTCATCGAGCGATACGGCGGAGAATAATTAAGACGATCAGTTTGAAAGCTCGCCTAGTACAATCGGCCTGAGCTAGTCCAAGTCTGGCGTTTAGAGCCAATATCTAAGCCTCTTTTCCAACTTCGTATCAACCTGAATTCATTTGGGAATACTTTCTTCAAGGCCCGCAGTACGAAATACGAGCAGTACCACTTTCACTATGACAACCTCGGGCATTTTCCATATGCTTATTTAATCCCCTATGGTCGTCTCCGCCCTCATCTTCTGTCAAAATGTTACTCCCTAGACCCGCGCAACTTGATTCTCATCGTTTAGTTTCCCTTAAACGAAAAATGACGATAAGACCACCCGTAGACGCTGGATTACACACCCCAGTGGATTGTAATCTACGTCGGGAGAATCGCCAAATAAGTTTAGATTCCGTACC".as_bytes().to_vec();
-    let b = "TTACTGTATGAAAGGCTTGGTTATCTGCGGCCTTGTTCGTCGGGCCGGATCCGAAGTTAAGCCGCTGCCAAAGAACACCCTTCAATGATCGCTCGCGTGACCCACCGCCGGGTTTTCTTTCCTATTCGTTTTATACCCATAATAGTACGTAAAAAATTGGTCATCTTCTATTTGGACAATCGGAGTCGTCGGACTCCGGAAAACGACTTTCGAACCTATCTTGGCTAGGGATACTTCAGTTTTGCCGGGGACTCTGCCCCCCCATATAAGGAGTCCCTACGACCAAATCGCTACTTAAGTGTCATTGAGTAAGCGGTCTTCACCTAGTTCGGGTCGCGATGCGGATGGCCGATGGATCACACTTTCGCGTAAATACGGAATAGTCGGCTACTGGGCCTCATAGACGTTCGATCTATCGCTTACCGAAGTTCACCATATTTAACAAACTCTGACAAGGCTACAGCGGGGTCTCATGAAGAGAAGTCGTAAAACGGGATCGTGCATCTTGGGGGACAGCCTGCTAGACCAGAACTGGTACTGTCCCGTGAAATCCCTCCCTGCACGATCAAATTGACATAGAGTCACGAGCGATACGGCGGACGTATATGGAATAAGAGGATCAGTTTGAAAGCGTCGCCTAGTACAATCGGCCTGAGCTAGTCCAATCTGGCGTTTAAGACCAACATCTAGCCTCACTTCCAACTTCGTGTTCAACCTGATACTCCAGTTTGGGAATGACTTTCTTCAAGGCCCTTGCAGTACGAAATACGAGCAGTCCGCATTTCAACTATGGCACCTCCGGGCAATTAACATATGATTATTTAATCCCCTATGGTTCAGTCTCCGCCCTCACTCTCCTGTCAACAATGTTACTCCCTAGACCCGCGCAACTTAATTCTCATCGTTTAGTTTCCCGTTAAACGAAAAAGGACGATAAGAACCCACCCGTAGACGCTGGATTATACACCACTAGGTGGATTGTATCTACGTCGGAGAATCGCCAAATAGGTTAGATTCCGGTAGA".as_bytes().to_vec();
-    println!("Heuristic:\n{:?}", h);
-    println!("{}\n{}", to_string(&a), to_string(&b));
-    align(&a, &b, &alphabet, stats, h).print();
-    return;
-
-    {
-        // True on success.
-        let test = |start, end| {
-            std::panic::catch_unwind(|| {
-                align(
-                    &a[start..min(a.len(), end)].to_vec(),
-                    &b[start..min(b.len(), end)].to_vec(),
-                    &alphabet,
-                    stats,
-                    h,
-                )
-                .print()
-            })
-            .is_ok()
-        };
-        let start;
-        let mut end = max(a.len(), b.len());
-
-        // Binary search the start of the sequence in steps of l.
-        {
-            let mut left = 0;
-            let mut right = end;
-            while left / l < right / l {
-                let mid = (left + right) / 2 / l * l;
-                if test(mid, end) {
-                    right = mid;
-                } else {
-                    left = mid;
-                }
-            }
-            start = left;
-        }
-        // Binary search the end of the sequence.
-        {
-            let mut left = start;
-            let mut right = end;
-            while left < right {
-                let mid = (left + right) / 2;
-                if test(start, mid) {
-                    left = mid;
-                } else {
-                    right = mid;
-                }
-            }
-            end = left;
-        }
-        assert!(!test(start, end));
     }
+    .equal_to_seed_heuristic();
+
+    let (a, b, alphabet, stats) = setup(n, e);
+    println!("Heuristic:\n{:?}", h);
+
+    // True on success.
+    let test = |start, end| {
+        println!("Test: {} {}", start, end);
+        let v = std::panic::catch_unwind(|| {
+            align(
+                &a[start..min(a.len(), end)].to_vec(),
+                &b[start..min(b.len(), end)].to_vec(),
+                &alphabet,
+                stats,
+                h,
+            )
+            .print()
+        })
+        .is_ok();
+        println!("Test: {} {} => {}", start, end, v);
+        v
+    };
+    let start;
+    let mut end = max(a.len(), b.len());
+
+    // Binary search the start of the sequence in steps of l.
+    {
+        let mut left = 0;
+        let mut right = end;
+        while left / l < right / l {
+            let mid = (left + right) / 2 / l * l;
+            if test(mid, end) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        start = left;
+    }
+    // Binary search the end of the sequence.
+    {
+        let mut left = start;
+        let mut right = end;
+        while left < right {
+            let mid = (left + right) / 2;
+            if test(start, mid) {
+                left = mid + l;
+            } else {
+                right = mid;
+            }
+        }
+        end = left;
+    }
+    assert!(!test(start, end));
+    println!("Result\n{}\n{}", to_string(&a), to_string(&b));
 }
