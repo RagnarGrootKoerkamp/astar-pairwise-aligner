c857237fda40196955e2aa9b5313989b819724a5
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-01-11 17:15:18 +0100

    Extract Algorithm from main.rs to algorithm.rs

---
+++ src/algorithms.rs
@@ -0,0 +1,172 @@
+use crate::prelude::*;
+use std::marker::PhantomData;
+use structopt::StructOpt;
+use strum_macros::EnumString;
+
+#[derive(EnumString, Default)]
+#[strum(ascii_case_insensitive)]
+enum Cost {
+    Zero,
+    #[default]
+    Gap,
+    Max,
+    Count,
+    BiCount,
+}
+
+#[derive(EnumString, Default)]
+#[strum(ascii_case_insensitive)]
+enum Contour {
+    BruteForce,
+    #[default]
+    LogQuery,
+    Set,
+}
+
+#[derive(EnumString)]
+#[strum(ascii_case_insensitive)]
+enum Contours {
+    BruteForce,
+    Naive,
+}
+
+impl Default for Contours {
+    fn default() -> Self {
+        Self::Naive
+    }
+}
+
+#[derive(EnumString)]
+#[strum(ascii_case_insensitive)]
+enum Algorithm {
+    // The basic n^2 DP
+    Naive,
+    // Naive, but with SIMD
+    Simd,
+    // SeedHeuristic, with the provided --cost
+    Seed,
+    // GapSeedHeuristic, using an efficient implementation from contours
+    GapSeed,
+}
+
+#[derive(StructOpt)]
+pub struct Params {
+    #[structopt(short, long, default_value = "GapSeed")]
+    algorithm: Algorithm,
+
+    #[structopt(short, default_value = "7")]
+    l: usize,
+
+    #[structopt(short, default_value = "1")]
+    max_seed_cost: usize,
+
+    #[structopt(long, default_value = "Gap")]
+    cost: Cost,
+
+    #[structopt(short = "-C", long, default_value = "Naive")]
+    contours: Contours,
+
+    #[structopt(short = "-c", long, default_value = "LogQuery")]
+    contour: Contour,
+
+    #[structopt(long)]
+    no_prune: bool,
+
+    #[structopt(long, default_value = "0.5")]
+    prune_fraction: f32,
+}
+
+pub fn run(a: &Sequence, b: &Sequence, params: &Params) {
+    //println!("{}\n{}", to_string(&a), to_string(&b));
+
+    match params.algorithm {
+        Algorithm::Naive => {
+            let dist = bio::alignment::distance::levenshtein(&a, &b);
+            println!("SIMD {:>8} {:>8} {:>6}", a.len(), b.len(), dist);
+        }
+        Algorithm::Simd => {
+            let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
+            println!("SIMD {:>8} {:>8} {:>6}", a.len(), b.len(), dist);
+        }
+        Algorithm::Seed => {
+            fn run_cost<C: Distance>(a: &Sequence, b: &Sequence, params: &Params)
+            where
+                for<'a> C::DistanceInstance<'a>: HeuristicInstance<'a, Pos = Pos>,
+            {
+                let heuristic = SeedHeuristic {
+                    match_config: MatchConfig {
+                        length: Fixed(params.l),
+                        max_match_cost: params.max_seed_cost,
+                        ..Default::default()
+                    },
+                    distance_function: C::default(),
+                    pruning: !params.no_prune,
+                    prune_fraction: params.prune_fraction,
+                };
+                println!("Heuristic:\n{:?}", heuristic);
+
+                let alphabet = Alphabet::new(b"ACTG");
+                let sequence_stats = SequenceStats {
+                    len_a: a.len(),
+                    len_b: b.len(),
+                    error_rate: 0.,
+                    source: Source::Extern,
+                };
+
+                align(&a, &b, &alphabet, sequence_stats, heuristic).print();
+            }
+
+            match params.cost {
+                Cost::Zero => run_cost::<ZeroCost>(a, b, params),
+                Cost::Gap => run_cost::<GapCost>(a, b, params),
+                Cost::Max => run_cost::<MaxCost>(a, b, params),
+                Cost::Count => run_cost::<CountCost>(a, b, params),
+                Cost::BiCount => run_cost::<BiCountCost>(a, b, params),
+            }
+        }
+        Algorithm::GapSeed => {
+            fn run_contours<C: crate::contour::Contours>(
+                a: &Sequence,
+                b: &Sequence,
+                params: &Params,
+            ) {
+                let heuristic = GapSeedHeuristic {
+                    match_config: MatchConfig {
+                        length: Fixed(params.l),
+                        max_match_cost: params.max_seed_cost,
+                        ..Default::default()
+                    },
+                    pruning: !params.no_prune,
+                    prune_fraction: params.prune_fraction,
+                    c: PhantomData::<NaiveContours<BruteForceContour>>,
+                };
+                println!("Heuristic:\n{:?}", heuristic);
+
+                let alphabet = Alphabet::new(b"ACTG");
+                let sequence_stats = SequenceStats {
+                    len_a: a.len(),
+                    len_b: b.len(),
+                    error_rate: 0.,
+                    source: Source::Extern,
+                };
+
+                align(&a, &b, &alphabet, sequence_stats, heuristic).print();
+            }
+
+            match params.contours {
+                Contours::BruteForce => run_contours::<BruteForceContours>(a, b, params),
+                Contours::Naive => match params.contour {
+                    Contour::BruteForce => {
+                        run_contours::<NaiveContours<BruteForceContour>>(a, b, params)
+                    }
+                    Contour::LogQuery => {
+                        run_contours::<NaiveContours<LogQueryContour>>(a, b, params)
+                    }
+                    Contour::Set => run_contours::<NaiveContours<SetContour>>(a, b, params),
+                },
+            }
+        }
+    };
+
+    //align(&a, &b, &alphabet, sequence_stats, heuristic).print();
+}
--- src/main.rs
+++ src/main.rs
@@ -5,143 +5,16 @@ use std::{marker::PhantomData, path::PathBuf};
 use structopt::StructOpt;
 use strum_macros::EnumString;
 
-#[derive(EnumString, Default)]
-#[strum(ascii_case_insensitive)]
-enum Cost {
-    Zero,
-    #[default]
-    Gap,
-    Max,
-    Count,
-    BiCount,
-}
+#[derive(StructOpt)]
+struct Input {
+    #[structopt(short, long, parse(from_os_str))]
+    input: Option<PathBuf>,
 
-#[derive(EnumString, Default)]
-#[strum(ascii_case_insensitive)]
-enum Contour {
-    BruteForce,
-    #[default]
-    LogQuery,
-    Set,
-}
+    #[structopt(short, conflicts_with = "input", required_unless = "input")]
+    n: Option<usize>,
 
-#[derive(EnumString)]
-#[strum(ascii_case_insensitive)]
-enum Contours {
-    BruteForce,
-    Naive,
-}
-
-impl Default for Contours {
-    fn default() -> Self {
-        Self::Naive
-    }
-}
-
-#[derive(EnumString)]
-#[strum(ascii_case_insensitive)]
-enum Algorithm {
-    // The basic n^2 DP
-    Naive,
-    // Naive, but with SIMD
-    Simd,
-    // SeedHeuristic, with the provided --cost
-    Seed,
-    // GapSeedHeuristic, using an efficient implementation from contours
-    GapSeed,
-}
-
-fn run(a: &Sequence, b: &Sequence, args: &Cli) {
-    //println!("{}\n{}", to_string(&a), to_string(&b));
-
-    match args.algorithm {
-        Algorithm::Naive => {
-            let dist = bio::alignment::distance::levenshtein(&a, &b);
-            println!("SIMD {:>8} {:>8} {:>6}", a.len(), b.len(), dist);
-        }
-        Algorithm::Simd => {
-            let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
-            println!("SIMD {:>8} {:>8} {:>6}", a.len(), b.len(), dist);
-        }
-        Algorithm::Seed => {
-            fn run_cost<C: Distance>(a: &Sequence, b: &Sequence, args: &Cli)
-            where
-                for<'a> C::DistanceInstance<'a>: HeuristicInstance<'a, Pos = Pos>,
-            {
-                let heuristic = SeedHeuristic {
-                    match_config: pairwise_aligner::prelude::MatchConfig {
-                        length: Fixed(args.l),
-                        max_match_cost: args.max_seed_cost,
-                        ..Default::default()
-                    },
-                    distance_function: C::default(),
-                    pruning: !args.no_prune,
-                    prune_fraction: args.prune_fraction,
-                };
-                println!("Heuristic:\n{:?}", heuristic);
-
-                let alphabet = Alphabet::new(b"ACTG");
-                let sequence_stats = SequenceStats {
-                    len_a: a.len(),
-                    len_b: b.len(),
-                    error_rate: 0.,
-                    source: Source::Extern,
-                };
-
-                align(&a, &b, &alphabet, sequence_stats, heuristic).print();
-            }
-
-            match args.cost {
-                Cost::Zero => run_cost::<ZeroCost>(a, b, args),
-                Cost::Gap => run_cost::<GapCost>(a, b, args),
-                Cost::Max => run_cost::<MaxCost>(a, b, args),
-                Cost::Count => run_cost::<CountCost>(a, b, args),
-                Cost::BiCount => run_cost::<BiCountCost>(a, b, args),
-            }
-        }
-        Algorithm::GapSeed => {
-            fn run_contours<C: pairwise_aligner::prelude::Contours>(
-                a: &Sequence,
-                b: &Sequence,
-                args: &Cli,
-            ) {
-                let heuristic = GapSeedHeuristic {
-                    match_config: pairwise_aligner::prelude::MatchConfig {
-                        length: Fixed(args.l),
-                        max_match_cost: args.max_seed_cost,
-                        ..Default::default()
-                    },
-                    pruning: !args.no_prune,
-                    prune_fraction: args.prune_fraction,
-                    c: PhantomData::<NaiveContours<BruteForceContour>>,
-                };
-                println!("Heuristic:\n{:?}", heuristic);
-
-                let alphabet = Alphabet::new(b"ACTG");
-                let sequence_stats = SequenceStats {
-                    len_a: a.len(),
-                    len_b: b.len(),
-                    error_rate: 0.,
-                    source: Source::Extern,
-                };
-
-                align(&a, &b, &alphabet, sequence_stats, heuristic).print();
-            }
-
-            match args.contours {
-                Contours::BruteForce => run_contours::<BruteForceContours>(a, b, args),
-                Contours::Naive => match args.contour {
-                    Contour::BruteForce => {
-                        run_contours::<NaiveContours<BruteForceContour>>(a, b, args)
-                    }
-                    Contour::LogQuery => run_contours::<NaiveContours<LogQueryContour>>(a, b, args),
-                    Contour::Set => run_contours::<NaiveContours<SetContour>>(a, b, args),
-                },
-            }
-        }
-    };
-
-    //align(&a, &b, &alphabet, sequence_stats, heuristic).print();
+    #[structopt(short, default_value = "0.2")]
+    e: f32,
 }
 
 #[derive(StructOpt)]
@@ -151,45 +24,18 @@ fn run(a: &Sequence, b: &Sequence, args: &Cli) {
     author = "Ragnar Groot Koerkamp, Pesho Ivanov"
 )]
 struct Cli {
-    #[structopt(short, long, parse(from_os_str))]
-    input: Option<PathBuf>,
+    #[structopt(flatten)]
+    input: Input,
 
-    #[structopt(short, conflicts_with = "input", required_unless = "input")]
-    n: Option<usize>,
-
-    #[structopt(short, default_value = "0.2")]
-    e: f32,
-
-    #[structopt(short, default_value = "7")]
-    l: usize,
-
-    #[structopt(short, long, default_value = "GapSeed")]
-    algorithm: Algorithm,
-
-    #[structopt(long, default_value = "Gap")]
-    cost: Cost,
-
-    #[structopt(short = "-C", long, default_value = "Naive")]
-    contours: Contours,
-
-    #[structopt(short = "-c", long, default_value = "LogQuery")]
-    contour: Contour,
-
-    #[structopt(short, default_value = "1")]
-    max_seed_cost: usize,
-
-    #[structopt(long)]
-    no_prune: bool,
-
-    #[structopt(long, default_value = "0.5")]
-    prune_fraction: f32,
+    #[structopt(flatten)]
+    params: Params,
 }
 
 fn main() {
     let args = Cli::from_args();
 
     // Read the input
-    if let Some(input) = &args.input {
+    if let Some(input) = &args.input.input {
         let data = std::fs::read(&input).unwrap();
         let pairs = data
             .split(|c| *c == '\n' as u8)
@@ -202,12 +48,12 @@ fn main() {
             .collect_vec();
 
         for (a, b) in pairs {
-            run(&a, &b, &args);
+            run(&a, &b, &args.params);
         }
     } else {
         // Generate random input.
         // TODO: Propagate stats.
-        let (a, b, _, _) = setup(args.n.unwrap(), args.e);
-        run(&a, &b, &args);
+        let (a, b, _, _) = setup(args.input.n.unwrap(), args.input.e);
+        run(&a, &b, &args.params);
     }
 }
