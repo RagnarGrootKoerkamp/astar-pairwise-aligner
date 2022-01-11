66222c9c04251c557fe7a2b4c9dd0ec4ebbde56f
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-01-11 12:14:26 +0100

    Move integration tests to tests/

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,9 @@ pub mod heuristic;
 pub mod random_sequence;
 pub mod scored;
 pub mod seeds;
+pub mod test;
 pub mod thresholds;
 pub mod util;
-
-extern crate test;
 
 #[cfg(debug_assertions)]
 pub const DEBUG: bool = true;
@@ -63,11 +62,15 @@ pub mod prelude {
 
     pub(crate) use super::bucket_heap_impl as heap;
 
+    pub use super::*;
     pub use crate::contour::*;
     pub use crate::graph::*;
     pub use crate::heuristic::*;
+    pub use crate::random_sequence::*;
     pub use crate::seeds::{LengthConfig, LengthConfig::Fixed, Match, MatchConfig};
+    pub use crate::test::*;
     pub use crate::util::*;
+
     #[allow(unused_imports)]
     pub(crate) use crate::DEBUG;
 }
@@ -83,13 +86,13 @@ use std::{
     time,
 };
 
-use crate::random_sequence::{random_mutate, random_sequence};
 use prelude::*;
 
 #[derive(Serialize, Clone, Copy, Debug)]
 pub enum Source {
     Uniform,
     Manual,
+    Extern,
 }
 impl fmt::Display for Source {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -467,188 +470,5 @@ where
         },
         answer_cost: distance,
         path,
-    }
-}
-
-// For quick testing
-pub fn setup_with_seed(
-    n: usize,
-    e: f32,
-    seed: u64,
-) -> (Sequence, Sequence, Alphabet, SequenceStats) {
-    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(seed);
-    let alphabet = Alphabet::new(b"ACTG");
-    let a = random_sequence(n, &alphabet, &mut rng);
-    let b = random_mutate(&a, &alphabet, (n as f32 * e) as usize, &mut rng);
-
-    let sequence_stats = SequenceStats {
-        len_a: a.len(),
-        len_b: b.len(),
-        error_rate: e,
-        source: Source::Uniform,
-    };
-    (a, b, alphabet, sequence_stats)
-}
-pub fn setup(n: usize, e: f32) -> (Sequence, Sequence, Alphabet, SequenceStats) {
-    setup_with_seed(n, e, 31415)
-}
-
-pub fn test_heuristic<H: Heuristic, HI>(n: usize, e: f32, h: H) -> AlignResult
-where
-    H: for<'a> Heuristic<Instance<'a> = HI>,
-    HI: for<'a> HeuristicInstance<'a, Pos = Pos>,
-{
-    let (a, b, alphabet, stats) = setup(n, e);
-    align(&a, &b, &alphabet, stats, h)
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn bicount_admissible() {
-        let alphabet = &Alphabet::new(b"ACTG");
-
-        let _n = 25;
-        let _e = 0.2;
-        let l = 4;
-        let pattern = "AGACGTCC".as_bytes().to_vec();
-        let ___text = "AGACGTCCA".as_bytes().to_vec();
-        let text = ___text;
-
-        let stats = SequenceStats {
-            len_a: pattern.len(),
-            len_b: text.len(),
-            error_rate: 0.,
-            source: Source::Manual,
-        };
-
-        let r = align(
-            &pattern,
-            &text,
-            &alphabet,
-            stats,
-            SeedHeuristic {
-                match_config: MatchConfig {
-                    length: Fixed(l),
-                    max_match_cost: 1,
-                    ..MatchConfig::default()
-                },
-                distance_function: BiCountCost,
-                pruning: false,
-                ..SeedHeuristic::default()
-            },
-        );
-        assert!(r.heuristic_stats2.root_h <= r.answer_cost);
-    }
-
-    // Failed because of match distance > 0
-    #[test]
-    fn consistency_1() {
-        let h = GapSeedHeuristic {
-            match_config: MatchConfig {
-                length: Fixed(4),
-                max_match_cost: 1,
-                ..MatchConfig::default()
-            },
-            pruning: false,
-            c: PhantomData::<NaiveContours<BruteForceContour>>,
-            ..GapSeedHeuristic::default()
-        };
-        let (a, b, alphabet, stats) = setup(2000, 0.10);
-        let a = &a[361..369].to_vec();
-        let b = &b[363..371].to_vec();
-
-        println!("{}\n{}\n", to_string(&a), to_string(&b));
-        align(a, b, &alphabet, stats, h);
-    }
-
-    // Failed because of match distance > 0 and stricter consistency check
-    #[test]
-    fn consistency_2() {
-        let h = GapSeedHeuristic {
-            match_config: MatchConfig {
-                length: Fixed(5),
-                max_match_cost: 1,
-                ..MatchConfig::default()
-            },
-            pruning: false,
-            c: PhantomData::<NaiveContours<BruteForceContour>>,
-            ..GapSeedHeuristic::default()
-        };
-        let (a, b, alphabet, stats) = setup(2000, 0.10);
-        let a = &a[236..246].to_vec();
-        let b = &b[236..246].to_vec();
-
-        println!("{}\n{}\n", to_string(&a), to_string(&b));
-        align(a, b, &alphabet, stats, h);
-    }
-
-    // Failed because of pruning
-    #[test]
-    #[ignore]
-    fn consistency_3() {
-        let h = GapSeedHeuristic {
-            match_config: MatchConfig {
-                length: Fixed(4),
-                max_match_cost: 0,
-                ..MatchConfig::default()
-            },
-            pruning: true,
-            c: PhantomData::<NaiveContours<BruteForceContour>>,
-            ..GapSeedHeuristic::default()
-        };
-        let (a, b, alphabet, stats) = setup(2000, 0.10);
-        let a = &a.to_vec();
-        let b = &b.to_vec();
-
-        println!("{}\n{}\n", to_string(&a), to_string(&b));
-        align(a, b, &alphabet, stats, h);
-    }
-
-    // Failed because of pruning and match distance
-    #[test]
-    fn consistency_4() {
-        let h = GapSeedHeuristic {
-            match_config: MatchConfig {
-                length: Fixed(6),
-                max_match_cost: 1,
-                ..MatchConfig::default()
-            },
-            pruning: true,
-            c: PhantomData::<NaiveContours<BruteForceContour>>,
-            ..GapSeedHeuristic::default()
-        };
-        let (a, b, alphabet, stats) = setup(2000, 0.10);
-        let a = &a[846..870].to_vec();
-        let b = &b[856..880].to_vec();
-        // TTGTGGGCCCTCTTAACTTCCAAC
-        // TTTTTGGGCCCTTTAACTTCCAAC
-
-        println!("{}\n{}\n", to_string(&a), to_string(&b));
-        align(a, b, &alphabet, stats, h);
-    }
-
-    // Failed because of pruning and large edit distance
-    #[test]
-    fn consistency_5() {
-        let h = GapSeedHeuristic {
-            match_config: MatchConfig {
-                length: Fixed(4),
-                max_match_cost: 0,
-                ..MatchConfig::default()
-            },
-            pruning: true,
-            c: PhantomData::<NaiveContours<BruteForceContour>>,
-            ..GapSeedHeuristic::default()
-        };
-        let (a, b, alphabet, stats) = setup(2000, 0.20);
-        let a = &a[200..310].to_vec();
-        let b = &b[203..313].to_vec();
-
-        println!("{}\n{}\n", to_string(&a), to_string(&b));
-        align(a, b, &alphabet, stats, h);
     }
 }
---
+++ tests/consistency.rs
@@ -0,0 +1,146 @@
+use pairwise_aligner::prelude::*;
+
+#[test]
+fn bicount_admissible() {
+    let alphabet = &Alphabet::new(b"ACTG");
+
+    let _n = 25;
+    let _e = 0.2;
+    let l = 4;
+    let pattern = "AGACGTCC".as_bytes().to_vec();
+    let ___text = "AGACGTCCA".as_bytes().to_vec();
+    let text = ___text;
+
+    let stats = SequenceStats {
+        len_a: pattern.len(),
+        len_b: text.len(),
+        error_rate: 0.,
+        source: Source::Manual,
+    };
+
+    let r = align(
+        &pattern,
+        &text,
+        &alphabet,
+        stats,
+        SeedHeuristic {
+            match_config: MatchConfig {
+                length: Fixed(l),
+                max_match_cost: 1,
+                ..MatchConfig::default()
+            },
+            distance_function: BiCountCost,
+            pruning: false,
+            ..SeedHeuristic::default()
+        },
+    );
+    assert!(r.heuristic_stats2.root_h <= r.answer_cost);
+}
+
+// Failed because of match distance > 0
+#[test]
+fn consistency_1() {
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(4),
+            max_match_cost: 1,
+            ..MatchConfig::default()
+        },
+        pruning: false,
+        c: PhantomData::<NaiveContours<BruteForceContour>>,
+        ..GapSeedHeuristic::default()
+    };
+    let (a, b, alphabet, stats) = setup(2000, 0.10);
+    let a = &a[361..369].to_vec();
+    let b = &b[363..371].to_vec();
+
+    println!("{}\n{}\n", to_string(&a), to_string(&b));
+    align(a, b, &alphabet, stats, h);
+}
+
+// Failed because of match distance > 0 and stricter consistency check
+#[test]
+fn consistency_2() {
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(5),
+            max_match_cost: 1,
+            ..MatchConfig::default()
+        },
+        pruning: false,
+        c: PhantomData::<NaiveContours<BruteForceContour>>,
+        ..GapSeedHeuristic::default()
+    };
+    let (a, b, alphabet, stats) = setup(2000, 0.10);
+    let a = &a[236..246].to_vec();
+    let b = &b[236..246].to_vec();
+
+    println!("{}\n{}\n", to_string(&a), to_string(&b));
+    align(a, b, &alphabet, stats, h);
+}
+
+// Failed because of pruning
+#[test]
+#[ignore]
+fn consistency_3() {
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(4),
+            max_match_cost: 0,
+            ..MatchConfig::default()
+        },
+        pruning: true,
+        c: PhantomData::<NaiveContours<BruteForceContour>>,
+        ..GapSeedHeuristic::default()
+    };
+    let (a, b, alphabet, stats) = setup(2000, 0.10);
+    let a = &a.to_vec();
+    let b = &b.to_vec();
+
+    println!("{}\n{}\n", to_string(&a), to_string(&b));
+    align(a, b, &alphabet, stats, h);
+}
+
+// Failed because of pruning and match distance
+#[test]
+fn consistency_4() {
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(6),
+            max_match_cost: 1,
+            ..MatchConfig::default()
+        },
+        pruning: true,
+        c: PhantomData::<NaiveContours<BruteForceContour>>,
+        ..GapSeedHeuristic::default()
+    };
+    let (a, b, alphabet, stats) = setup(2000, 0.10);
+    let a = &a[846..870].to_vec();
+    let b = &b[856..880].to_vec();
+    // TTGTGGGCCCTCTTAACTTCCAAC
+    // TTTTTGGGCCCTTTAACTTCCAAC
+
+    println!("{}\n{}\n", to_string(&a), to_string(&b));
+    align(a, b, &alphabet, stats, h);
+}
+
+// Failed because of pruning and large edit distance
+#[test]
+fn consistency_5() {
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(4),
+            max_match_cost: 0,
+            ..MatchConfig::default()
+        },
+        pruning: true,
+        c: PhantomData::<NaiveContours<BruteForceContour>>,
+        ..GapSeedHeuristic::default()
+    };
+    let (a, b, alphabet, stats) = setup(2000, 0.20);
+    let a = &a[200..310].to_vec();
+    let b = &b[203..313].to_vec();
+
+    println!("{}\n{}\n", to_string(&a), to_string(&b));
+    align(a, b, &alphabet, stats, h);
+}
