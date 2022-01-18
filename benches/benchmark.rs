e3240eafb8eba1233d32a8d92618d2cd8f9da42a
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-01-18 13:13:47 +0100

    Add CentralContour, using caching of the last relevant dominant point

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -106,102 +106,102 @@ fn base_50000_similar(bench: &mut Bencher) {
     bench.iter(|| align(&a, &b, &alphabet, stats, h));
 }
 
-// #[bench]
-// fn optimal_hundred(bench: &mut Bencher) {
-//     let n = 100;
-//     let e = 0.2;
-//     let h = GapSeedHeuristic {
-//         match_config: MatchConfig {
-//             length: Fixed(6),
-//             max_match_cost: 1,
-//             ..MatchConfig::default()
-//         },
-//         pruning: true,
-//         prune_fraction: 1.0,
-//         c: PhantomData::<NaiveContours<CentralContour>>,
-//         ..GapSeedHeuristic::default()
-//     };
+#[bench]
+fn fast_100(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.2;
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(6),
+            max_match_cost: 1,
+            ..MatchConfig::default()
+        },
+        pruning: true,
+        prune_fraction: 1.0,
+        c: PhantomData::<NaiveContours<CentralContour>>,
+        ..GapSeedHeuristic::default()
+    };
 
-//     let (a, b, alphabet, stats) = setup(n, e);
-//     bench.iter(|| align(&a, &b, &alphabet, stats, h));
-// }
+    let (a, b, alphabet, stats) = setup(n, e);
+    bench.iter(|| align(&a, &b, &alphabet, stats, h));
+}
 
-// #[bench]
-// fn optimal_thousand(bench: &mut Bencher) {
-//     let n = 1000;
-//     let e = 0.2;
-//     let h = GapSeedHeuristic {
-//         match_config: MatchConfig {
-//             length: Fixed(7),
-//             max_match_cost: 1,
-//             ..MatchConfig::default()
-//         },
-//         pruning: true,
-//         prune_fraction: 1.0,
-//         c: PhantomData::<NaiveContours<CentralContour>>,
-//         ..GapSeedHeuristic::default()
-//     };
+#[bench]
+fn fast_1000(bench: &mut Bencher) {
+    let n = 1000;
+    let e = 0.2;
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(7),
+            max_match_cost: 1,
+            ..MatchConfig::default()
+        },
+        pruning: true,
+        prune_fraction: 1.0,
+        c: PhantomData::<NaiveContours<CentralContour>>,
+        ..GapSeedHeuristic::default()
+    };
 
-//     let (a, b, alphabet, stats) = setup(n, e);
-//     bench.iter(|| align(&a, &b, &alphabet, stats, h));
-// }
+    let (a, b, alphabet, stats) = setup(n, e);
+    bench.iter(|| align(&a, &b, &alphabet, stats, h));
+}
 
-// #[bench]
-// fn optimal_ten_k(bench: &mut Bencher) {
-//     let n = 10000;
-//     let e = 0.2;
-//     let h = GapSeedHeuristic {
-//         match_config: MatchConfig {
-//             length: Fixed(8),
-//             max_match_cost: 1,
-//             ..MatchConfig::default()
-//         },
-//         pruning: true,
-//         prune_fraction: 1.0,
-//         c: PhantomData::<NaiveContours<CentralContour>>,
-//         ..GapSeedHeuristic::default()
-//     };
+#[bench]
+fn fast_10000(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.2;
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(8),
+            max_match_cost: 1,
+            ..MatchConfig::default()
+        },
+        pruning: true,
+        prune_fraction: 1.0,
+        c: PhantomData::<NaiveContours<CentralContour>>,
+        ..GapSeedHeuristic::default()
+    };
 
-//     let (a, b, alphabet, stats) = setup(n, e);
-//     bench.iter(|| align(&a, &b, &alphabet, stats, h));
-// }
+    let (a, b, alphabet, stats) = setup(n, e);
+    bench.iter(|| align(&a, &b, &alphabet, stats, h));
+}
 
-// #[bench]
-// fn optimal_ten_k_prune_less(bench: &mut Bencher) {
-//     let n = 10000;
-//     let e = 0.2;
-//     let h = GapSeedHeuristic {
-//         match_config: MatchConfig {
-//             length: Fixed(8),
-//             max_match_cost: 1,
-//             ..MatchConfig::default()
-//         },
-//         pruning: true,
-//         prune_fraction: 0.6,
-//         c: PhantomData::<NaiveContours<CentralContour>>,
-//         ..GapSeedHeuristic::default()
-//     };
+#[bench]
+fn fast_10000_prune_less(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.2;
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(8),
+            max_match_cost: 1,
+            ..MatchConfig::default()
+        },
+        pruning: true,
+        prune_fraction: 0.6,
+        c: PhantomData::<NaiveContours<CentralContour>>,
+        ..GapSeedHeuristic::default()
+    };
 
-//     let (a, b, alphabet, stats) = setup(n, e);
-//     bench.iter(|| align(&a, &b, &alphabet, stats, h));
-// }
+    let (a, b, alphabet, stats) = setup(n, e);
+    bench.iter(|| align(&a, &b, &alphabet, stats, h));
+}
 
-// #[bench]
-// fn optimal_hundred_k_similar(bench: &mut Bencher) {
-//     let n = 100000;
-//     let e = 0.05;
-//     let h = GapSeedHeuristic {
-//         match_config: MatchConfig {
-//             length: Fixed(10),
-//             max_match_cost: 1,
-//             ..MatchConfig::default()
-//         },
-//         pruning: true,
-//         prune_fraction: 1.0,
-//         c: PhantomData::<NaiveContours<CentralContour>>,
-//         ..GapSeedHeuristic::default()
-//     };
+#[bench]
+fn fast_50000_similar(bench: &mut Bencher) {
+    let n = 50000;
+    let e = 0.05;
+    let h = GapSeedHeuristic {
+        match_config: MatchConfig {
+            length: Fixed(10),
+            max_match_cost: 1,
+            ..MatchConfig::default()
+        },
+        pruning: true,
+        prune_fraction: 1.0,
+        c: PhantomData::<NaiveContours<CentralContour>>,
+        ..GapSeedHeuristic::default()
+    };
 
-//     let (a, b, alphabet, stats) = setup(n, e);
-//     bench.iter(|| align(&a, &b, &alphabet, stats, h));
-// }
+    let (a, b, alphabet, stats) = setup(n, e);
+    bench.iter(|| align(&a, &b, &alphabet, stats, h));
+}
