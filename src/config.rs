cde2f4f1bef6ee0d0bc2bf8cf0a176f1534cfda8
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-01-18 14:54:57 +0100

    Turn on USE_SHADOW_POINTS, it's slightly faster and makes code simpler

--- src/config.rs
+++ src/config.rs
@@ -9,7 +9,7 @@ pub const DEBUG: bool = false;
 
 /// If true: insert 'shadow' points in contours to make sure that contour v always contains contour v+1.
 /// This makes contour[v].query(p) monotone in v, allowing simpler binary search/query operations.
-pub const USE_SHADOW_POINTS: bool = false;
+pub const USE_SHADOW_POINTS: bool = true;
 
 /// Whenever A* pops a position, if the value of h and f is outdated, the point is pushed and not expanded.
 pub const RETRY_OUDATED_HEURISTIC_VALUE: bool = true;
