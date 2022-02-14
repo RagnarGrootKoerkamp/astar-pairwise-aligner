e18162b53e94599640b7760ae97bf5f4dfb2c328
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-02-14 10:41:09 +0100

    Merge matches and sorting benchmarks into one file to remove aho-corasick dependency

--- benches/matches.rs
+++ benches/matches.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
 #![feature(test)]
 #![cfg(test)]
 use pairwise_aligner::{
@@ -5,12 +7,174 @@ use pairwise_aligner::{
     seeds::{find_matches_qgram_hash_exact, find_matches_qgramindex, find_matches_trie},
 };
 
+#[macro_use]
+extern crate lazy_static;
 extern crate test;
 
 use test::Bencher;
 
 const E: f32 = 0.02;
 const K: I = 8;
+
+mod matches {
+
+    use aho_corasick::AhoCorasickBuilder;
+    use bio::data_structures::suffix_array::suffix_array;
+    use bio_types::sequence::Sequence;
+    use itertools::Itertools;
+    use suffix::SuffixTable;
+
+    use crate::{prelude::*, trie::Trie};
+
+    lazy_static! {
+        static ref ALPH: Alphabet = Alphabet::new(b"ACTG");
+        static ref TRANSFORM: RankTransform = RankTransform::new(&ALPH);
+    }
+
+    /// Some options for finding all matches between a[i*k, (i+1)*k) and b:
+    /// - qgramindex on b, query all seeds
+    /// - qgramindex on a, query all b
+    /// - suffixarray on b, sort/suffixarray a, sliding window
+    /// - suffixarray on b, query all a in log time each
+    /// - sort all a, query all b in log time each
+    /// - suffix automaton on full a, do incremental queries from b using back edges
+    /// - trie on b/a, query a/b.
+    /// - suffixarray on b, then build trie from sorted input and query all of a.
+    pub fn exact_matches(a: &Sequence, b: &Sequence, k: I) {}
+    pub fn sort_seeds(a: &Sequence, b: &Sequence, k: I) {
+        let mut seeds = a.chunks_exact(k as usize).collect_vec();
+        seeds.sort_unstable();
+    }
+    pub fn suffix_array_sort(a: &Sequence, b: &Sequence, k: I) {
+        let mut suffixes = b.windows(k as usize).collect_vec();
+        suffixes.sort_unstable();
+    }
+    pub fn suffix_array_bio(a: &Sequence, b: &Sequence, k: I) {
+        let mut b = b.to_vec();
+        b.push('$' as u8);
+        suffix_array(&b);
+    }
+    pub fn suffix_array_suffixtable(a: &Sequence, b: &Sequence, k: I) {
+        let st = SuffixTable::new(to_string(b));
+    }
+
+    pub fn aho_corasick(a: &Sequence, b: &Sequence, k: I) {
+        let ac = AhoCorasickBuilder::new()
+            .build_with_size::<u16, _, _>(a.chunks_exact(k as usize))
+            .unwrap();
+        let cnt = ac.find_overlapping_iter(b).count();
+        println!("{cnt}");
+    }
+
+    pub fn regex(a: &Sequence, b: &Sequence, k: I) {
+        let ac = AhoCorasickBuilder::new()
+            .build_with_size::<u16, _, _>(a.chunks_exact(k as usize))
+            .unwrap();
+        let cnt = ac.find_overlapping_iter(b).count();
+        println!("{cnt}");
+    }
+
+    pub fn build_trie(a: &Sequence, b: &Sequence, k: I) {
+        Trie::new(
+            b.windows(k as usize)
+                .enumerate()
+                .map(|(i, w)| (w, i as trie::Data)),
+            &ALPH,
+        );
+    }
+
+    pub fn build_trie_on_seeds(a: &Sequence, b: &Sequence, k: I) {
+        Trie::new(
+            a.chunks_exact(k as usize)
+                .enumerate()
+                .map(|(i, w)| (w, i as trie::Data)),
+            &ALPH,
+        );
+    }
+
+    pub fn build_trie_sorted(a: &Sequence, b: &Sequence, k: I) {
+        Trie::new(
+            b.windows(k as usize)
+                .enumerate()
+                .map(|(i, w)| (w, i as trie::Data)),
+            &ALPH,
+        );
+    }
+
+    pub fn build_trie_on_seeds_sorted(a: &Sequence, b: &Sequence, k: I) {
+        Trie::new(
+            a.chunks_exact(k as usize)
+                .enumerate()
+                .map(|(i, w)| (w, i as trie::Data)),
+            &ALPH,
+        );
+    }
+
+    pub fn seed_qgrams(a: &Sequence, b: &Sequence, k: I) {
+        todo!("Manual implementation needed");
+    }
+    pub fn suffix_qgrams(a: &Sequence, b: &Sequence, k: I) {
+        QGramIndex::new(k as u32, b, &ALPH);
+    }
+
+    pub fn seed_hashmap(a: &Sequence, b: &Sequence, k: I) {
+        let mut m = HashMap::<&[u8], u32>::default();
+        m.reserve(a.len());
+        for (i, w) in a.chunks_exact(k as usize).enumerate() {
+            *m.entry(w).or_default() = i as u32
+        }
+    }
+    pub fn suffix_hashmap(a: &Sequence, b: &Sequence, k: I) {
+        let mut m = HashMap::<&[u8], u32>::default();
+        m.reserve(b.len());
+        for (i, w) in b.windows(k as usize).enumerate() {
+            *m.entry(w).or_default() = i as u32
+        }
+    }
+
+    pub fn seed_hashmap_qgrams(a: &Sequence, b: &Sequence, k: I) {
+        let mut m = HashMap::<u32, u32>::default();
+        m.reserve(a.len());
+        for (i, w) in TRANSFORM.qgrams(k, a).step_by(k as usize).enumerate() {
+            *m.entry(w as u32).or_default() = i as u32;
+        }
+    }
+    pub fn suffix_hashmap_qgrams(a: &Sequence, b: &Sequence, k: I) {
+        let mut m = HashMap::<u32, u32>::default();
+        m.reserve(b.len());
+        for (i, w) in TRANSFORM.qgrams(k, b).enumerate() {
+            *m.entry(w as u32).or_default() = i as u32;
+        }
+    }
+
+    pub fn lookup_seeds_in_qgram_hashmap(a: &Sequence, b: &Sequence, k: I) {
+        let mut m = HashMap::<u32, u32>::default();
+        m.reserve(b.len());
+        for (i, w) in TRANSFORM.qgrams(k, b).enumerate() {
+            *m.entry(w as u32).or_default() = i as u32;
+        }
+        let mut cnt = 0;
+        for (j, w) in TRANSFORM.qgrams(k, a).step_by(k as usize).enumerate() {
+            if m.contains_key(&(w as u32)) {
+                cnt += 1;
+            }
+        }
+    }
+
+    pub fn lookup_suffixes_in_qgram_hashmap(a: &Sequence, b: &Sequence, k: I) {
+        let mut m = HashMap::<u32, u32>::default();
+        m.reserve(a.len());
+        for (i, w) in TRANSFORM.qgrams(k, a).step_by(k as usize).enumerate() {
+            *m.entry(w as u32).or_default() = i as u32;
+        }
+        let mut cnt = 0;
+        for (j, w) in TRANSFORM.qgrams(k, b).enumerate() {
+            if m.contains_key(&(w as u32)) {
+                cnt += 1;
+            }
+        }
+    }
+}
 
 #[bench]
 fn n100_exact_qgramindex(bench: &mut Bencher) {
@@ -243,5 +407,212 @@ fn n10000_lookup_suffixes_in_qgram_hashmap(bench: &mut Bencher) {
     let (a, b, _, _) = setup(n, E);
     bench.iter(|| {
         matches::lookup_suffixes_in_qgram_hashmap(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_b_suffix_array(bench: &mut Bencher) {
+    let n = 100;
+    let (a, b, _, _) = setup(n, 0.0);
+    bench.iter(|| {
+        matches::suffix_array_bio(&a, &b, 0);
+    });
+}
+
+#[bench]
+fn n10000_b_suffix_array(bench: &mut Bencher) {
+    let n = 10000;
+    let (a, b, _, _) = setup(n, 0.0);
+    bench.iter(|| {
+        matches::suffix_array_bio(&a, &b, 0);
+    });
+}
+
+#[bench]
+fn n100_b_suffix_array_2(bench: &mut Bencher) {
+    let n = 100;
+    let (a, b, _, _) = setup(n, 0.0);
+    bench.iter(|| {
+        matches::suffix_array_suffixtable(&a, &b, 0);
+    });
+}
+
+#[bench]
+fn n10000_b_suffix_array_2(bench: &mut Bencher) {
+    let n = 10000;
+    let (a, b, _, _) = setup(n, 0.0);
+    bench.iter(|| {
+        matches::suffix_array_suffixtable(&a, &b, 0);
+    });
+}
+
+#[bench]
+fn n100_b_suffix_array_sort(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::suffix_array_sort(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_b_suffix_array_sort(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::suffix_array_sort(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_a_sort_seeds(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::sort_seeds(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_a_sort_seeds(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::sort_seeds(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_b_build_trie(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::build_trie(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_b_build_trie(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::build_trie(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_a_build_trie_on_seeds(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::build_trie_on_seeds(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_a_build_trie_on_seeds(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::build_trie_on_seeds(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_b_qgramindex(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::suffix_qgrams(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_b_qgramindex(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::suffix_qgrams(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_b_hashmap(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::suffix_hashmap(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_b_hashmap(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::suffix_hashmap(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_a_hashmap(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::seed_hashmap(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_a_hashmap(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::seed_hashmap(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_b_hashmap_qgrams(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::suffix_hashmap_qgrams(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_b_hashmap_qgrams(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::suffix_hashmap_qgrams(&a, &b, K);
+    });
+}
+
+#[bench]
+fn n100_a_hashmap_qgrams(bench: &mut Bencher) {
+    let n = 100;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::seed_hashmap_qgrams(&a, &b, K);
+    });
+}
+#[bench]
+fn n10000_a_hashmap_qgrams(bench: &mut Bencher) {
+    let n = 10000;
+    let e = 0.01;
+    let (a, b, _, _) = setup(n, e);
+    bench.iter(|| {
+        matches::seed_hashmap_qgrams(&a, &b, K);
     });
 }
