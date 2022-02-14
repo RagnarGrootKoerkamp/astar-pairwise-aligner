87ac9cb7f2d82f201cdde50d1c0116112d8bc6a9
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-02-14 14:38:43 +0100

    Mode cost_model into graph

--- src/graph.rs
+++ src/graph.rs
@@ -206,3 +206,29 @@ impl<'a> AlignmentGraph<'a> {
         }
     }
 }
+
+/// A datatype for the cost of a match, possibly smaller than `Cost`.
+pub type MatchCost = u8;
+
+/// The costs to use for the distance computation.
+/// TODO: Gap-Affine costs.
+pub struct CostModel {
+    pub mismatch: MatchCost,
+    pub insertion: MatchCost,
+    pub deletion: MatchCost,
+}
+
+/// Default costs for EditDistance:
+/// mismatch, insertion, and deletion all cost 1.
+pub const EDIT_DISTANCE_COSTS: CostModel = CostModel {
+    mismatch: 1,
+    insertion: 1,
+    deletion: 1,
+};
+
+/// LCS corresponds to disallowing mismatches.
+pub const LCS_COSTS: CostModel = CostModel {
+    mismatch: MatchCost::MAX,
+    insertion: 1,
+    deletion: 1,
+};
