4ed07e7a9b0552431221f68e14e87ac755d060f5
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-02-14 15:26:14 +0100

    Rename module: algorithm -> runner

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@
     drain_filter
 )]
 
-pub mod algorithms;
 pub mod align;
 pub mod alignment_graph;
 pub mod astar;
@@ -21,28 +20,26 @@ pub mod datastructures;
 pub mod generate;
 pub mod heuristic;
 pub mod matches;
+pub mod runner;
 
 pub mod prelude {
-    pub use bio_types::sequence::Sequence;
-    pub use std::marker::PhantomData;
-
-    pub use rustc_hash::FxHashMap as HashMap;
-    pub use rustc_hash::FxHashSet as HashSet;
-
-    pub use config::*;
-
     pub use super::*;
-    pub use crate::algorithms::*;
     pub use crate::align::*;
     pub use crate::alignment_graph::*;
+    pub use crate::config::*;
     pub use crate::contour::*;
     pub use crate::datastructures::*;
     pub use crate::generate::*;
     pub use crate::heuristic::*;
-    pub use crate::matches::{LengthConfig, LengthConfig::Fixed, Match, MatchConfig};
+    pub use crate::matches::{LengthConfig::Fixed, *};
+    pub use crate::runner::*;
     pub use bio::alphabets::{Alphabet, RankTransform};
     pub use bio::data_structures::qgram_index::QGramIndex;
+    pub use bio_types::sequence::Sequence;
+    pub use rustc_hash::FxHashMap as HashMap;
+    pub use rustc_hash::FxHashSet as HashSet;
     pub use std::cmp::{max, min};
+    pub use std::marker::PhantomData;
 
     pub fn to_string(seq: &[u8]) -> String {
         String::from_utf8(seq.to_vec()).unwrap()
