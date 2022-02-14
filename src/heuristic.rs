bf8b20a53aa30a424cbd009e10444ca343ed54ab
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-02-14 12:13:07 +0100

    DELETE CONTOUR_GRAPH AND FAST_ZERO_SEED_HEURISTIC

--- src/heuristic.rs
+++ src/heuristic.rs
@@ -1,13 +1,11 @@
 pub mod distance;
 pub mod equal_heuristic;
-//pub mod fast_zero_seed_heuristic;
 pub mod gap_seed_heuristic;
 pub mod print;
 pub mod seed_heuristic;
 
 pub use distance::*;
 pub use equal_heuristic::*;
-//pub use fast_zero_seed_heuristic::*;
 pub use gap_seed_heuristic::*;
 pub use seed_heuristic::*;
 
