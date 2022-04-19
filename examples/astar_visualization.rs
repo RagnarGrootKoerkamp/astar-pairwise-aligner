037657ac8f5101cfe722de71fbb61fad6917cab1
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-04-19 23:57:18 +0200

    New 20% visualizations, and dijkstra-nogreedy

--- examples/astar_visualization.rs
+++ examples/astar_visualization.rs
@@ -2,13 +2,20 @@ use pairwise_aligner::prelude::*;
 
 fn main() {
     let n = 500;
-    let e = 0.3;
+    let e = 0.2;
 
     {
         let h = ZeroCost;
         let (a, b, alphabet, stats) = setup(n, e);
         let r = align(&a, &b, &alphabet, stats, h);
         r.write_explored_states("evals/astar-visualization/dijkstra.csv");
+        r.print();
+    }
+    {
+        let h = ZeroCost;
+        let (a, b, alphabet, stats) = setup(n, e);
+        let r = align_advanced(&a, &b, &alphabet, stats, h, false);
+        r.write_explored_states("evals/astar-visualization/dijkstra_nogreedy.csv");
         r.print();
     }
     {
