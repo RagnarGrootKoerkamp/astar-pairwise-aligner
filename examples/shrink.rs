632b89c79bbb049c925cc7ab631804150463715f
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-05-01 17:04:57 +0200

    Clean up examples (shrink/fuzz/print)

--- examples/shrink.rs
+++ examples/shrink.rs
@@ -1,6 +1,6 @@
 use std::panic::AssertUnwindSafe;
 
-use pairwise_aligner::prelude::{UnorderedHeuristic, *};
+use pairwise_aligner::prelude::*;
 
 fn main() {
     PRINT.store(false, std::sync::atomic::Ordering::Relaxed);
