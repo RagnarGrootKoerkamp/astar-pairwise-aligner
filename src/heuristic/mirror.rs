b4bbb3d36f9320ab93cdfc202d7beacf8654dcfd
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-05-05 17:51:51 +0200

    ADD {Mirror,Symmetric}Heuristic

---
+++ src/heuristic/mirror.rs
@@ -0,0 +1,79 @@
+use crate::prelude::*;
+
+#[derive(Debug, Clone, Copy)]
+pub struct MirrorHeuristic<H: Heuristic> {
+    pub h: H,
+}
+
+pub struct MirrorHeuristicI<'a, H: Heuristic> {
+    h: H::Instance<'a>,
+}
+
+impl<H: Heuristic> Heuristic for MirrorHeuristic<H>
+where
+    for<'a> H::Instance<'a>: HeuristicInstance<'a>,
+    Pos: Copy + Eq + std::fmt::Debug + Default,
+{
+    type Instance<'a> = MirrorHeuristicI<'a, H>;
+
+    fn name(&self) -> String {
+        "mirror(".to_owned() + &self.h.name() + ")"
+    }
+
+    fn build<'a>(
+        &self,
+        a: &'a bio_types::sequence::Sequence,
+        b: &'a bio_types::sequence::Sequence,
+        alphabet: &bio::alphabets::Alphabet,
+    ) -> Self::Instance<'a> {
+        MirrorHeuristicI {
+            h: self.h.build(b, a, alphabet),
+        }
+    }
+
+    fn params(&self) -> HeuristicParams {
+        HeuristicParams {
+            name: self.name(),
+            ..self.h.params()
+        }
+    }
+}
+
+impl<'a, H: Heuristic> HeuristicInstance<'a> for MirrorHeuristicI<'a, H>
+where
+    H::Instance<'a>: HeuristicInstance<'a>,
+{
+    fn h(&self, pos: Pos) -> Cost {
+        self.h.h(pos.mirror())
+    }
+
+    type Hint = <<H as Heuristic>::Instance<'a> as HeuristicInstance<'a>>::Hint;
+
+    fn is_seed_start_or_end(&self, pos: Pos) -> bool {
+        self.h.is_seed_start_or_end(pos.mirror())
+    }
+
+    fn prune(&mut self, pos: Pos, hint: Self::Hint, seed_cost: MatchCost) -> Cost {
+        self.h.prune(pos.mirror(), hint, seed_cost)
+    }
+
+    fn h_with_hint(&self, pos: Pos, hint: Self::Hint) -> (Cost, Self::Hint) {
+        self.h.h_with_hint(pos.mirror(), hint)
+    }
+
+    fn root_state(&self, root_pos: Pos) -> Self::Hint {
+        self.h.root_state(root_pos.mirror())
+    }
+
+    fn stats(&self) -> HeuristicStats {
+        self.h.stats()
+    }
+
+    fn root_potential(&self) -> Cost {
+        self.h.root_potential()
+    }
+
+    fn explore(&mut self, pos: Pos) {
+        self.h.explore(pos.mirror())
+    }
+}
