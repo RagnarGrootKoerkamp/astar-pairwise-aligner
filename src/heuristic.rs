60160c195d8796a0b2c46259b363c9afdd68f73e
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-05-05 16:03:12 +0200

    ADD HeuristicInstance::DISPLAY to show h/contour plot

--- src/heuristic.rs
+++ src/heuristic.rs
@@ -2,8 +2,9 @@ pub mod bruteforce_csh;
 pub mod chained_seed_heuristic;
 pub mod distance;
 pub mod equal_heuristic;
-pub mod print;
 pub mod seed_heuristic;
+
+use std::time::Duration;
 
 pub use bruteforce_csh::*;
 pub use chained_seed_heuristic::*;
@@ -11,6 +12,7 @@ pub use distance::*;
 pub use equal_heuristic::*;
 use rand::{prelude::Distribution, SeedableRng};
 use rand_chacha::ChaCha8Rng;
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Point};
 pub use seed_heuristic::*;
 
 use serde::Serialize;
@@ -71,6 +73,11 @@ pub trait Heuristic: std::fmt::Debug + Copy {
             ..Default::default()
         }
     }
+}
+
+pub enum DisplayType {
+    Heuristic,
+    Contours,
 }
 
 /// An instantiation of a heuristic for a specific pair of sequences.
@@ -187,6 +194,110 @@ pub trait HeuristicInstance<'a> {
                 termion::color::Fg(termion::color::Reset),
                 termion::color::Bg(termion::color::Reset)
             );
+        }
+    }
+
+    // hmax is used to cap the color gradient.
+    fn display(&self, target: Pos, display_type: DisplayType, max_val: Option<Cost>) {
+        //println!("Root h: {}", h.h(Pos(0, 0)));
+
+        let low = Pos(0, 0);
+        let high = target;
+
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+        video_subsystem.gl_attr().set_double_buffer(true);
+        let canvas_size = Point::new(
+            min(1000, high.0 - low.0 + 1) as i32,
+            min(1000, high.1 - low.1 + 1) as i32,
+        );
+
+        let pos_to_point = |Pos(x, y): Pos| -> Point {
+            //return Point::new(x as i32, y as i32);
+            Point::new(
+                ((x - low.0) as usize * (canvas_size.x() + 1) as usize
+                    / (high.0 - low.0 + 1) as usize) as i32,
+                ((y - low.1) as usize * (canvas_size.y() + 1) as usize
+                    / (high.1 - low.1 + 1) as usize) as i32,
+            )
+        };
+
+        let point_to_pos = |p: Point| -> Pos {
+            //return Pos(p.x() as u32, p.y() as u32);
+            Pos(
+                low.0
+                    + (p.x() as usize * (high.0 - low.0 + 1) as usize / canvas_size.x() as usize)
+                        as u32,
+                low.1
+                    + (p.y() as usize * (high.1 - low.1 + 1) as usize / canvas_size.y() as usize)
+                        as u32,
+            )
+        };
+
+        let window = video_subsystem
+            .window(
+                "rust-sdl2 demo",
+                canvas_size.x() as u32,
+                canvas_size.y() as u32,
+            )
+            .position_centered()
+            .build()
+            .unwrap();
+        let mut canvas = window.into_canvas().build().unwrap();
+        canvas.set_draw_color(Color::WHITE);
+        canvas.clear();
+
+        fn from_to_gradient(f: f32, c1: Color, c2: Color) -> Color {
+            let frac = |a: u8, b: u8| -> u8 { ((1. - f) * a as f32 + f * b as f32) as u8 };
+
+            return Color::RGB(frac(c1.r, c2.r), frac(c1.g, c2.g), frac(c1.b, c2.b));
+        }
+
+        let val = |pos| match display_type {
+            DisplayType::Heuristic => self.h(pos),
+            DisplayType::Contours => self.contour_value(pos),
+        };
+
+        let max_val = max_val.unwrap_or_else(|| max(val(Pos(0, 0)), val(Pos(0, target.1))));
+
+        // Draw background in color, matches in black.
+        let gradient = |h: Cost| -> Color {
+            if h <= max_val {
+                from_to_gradient(h as f32 / max_val as f32, Color::WHITE, Color::BLACK)
+            } else {
+                Color::GRAY
+            }
+        };
+        for i in 0..canvas_size.x {
+            for j in 0..canvas_size.y {
+                let point = Point::new(i as i32, j as i32);
+                let v = val(point_to_pos(point));
+                canvas.set_draw_color(gradient(v));
+                canvas.draw_point(point).unwrap();
+            }
+        }
+
+        canvas.present();
+        'outer: loop {
+            for event in sdl_context.event_pump().unwrap().poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Q),
+                        ..
+                    } => {
+                        todo!();
+                    }
+                    Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => {
+                        break 'outer;
+                    }
+                    _ => {}
+                }
+            }
+            ::std::thread::sleep(Duration::from_secs_f32(0.01));
         }
     }
 }
