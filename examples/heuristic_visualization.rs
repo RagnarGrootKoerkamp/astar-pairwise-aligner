3680093d151b57635e4cfb305981a39644e9f745
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-05-06 14:33:55 +0200

    Fix pruning

--- examples/heuristic_visualization.rs
+++ examples/heuristic_visualization.rs
@@ -56,7 +56,7 @@ fn main() {
             c: PhantomData::<HintContours<BruteForceContour>>::default(),
         };
         let mut h = Heuristic::build(&h, &a, &b, &alphabet);
-        //h.display(a, b, target, d, hmax, None, None, None);
+        //h.display(target, hmax, None, None, None);
         let graph = AlignmentGraph::new(a, b, true);
         let (distance_and_path, astar) = astar::astar(&graph, Pos(0, 0), &mut h);
         let (_distance, path) = distance_and_path.unwrap_or_default();
