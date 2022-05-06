a2ffc9ded7151cbd04e8eabb8fe81b63595d13c9
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-05-06 12:45:31 +0200

    add assert to fuzz

--- examples/fuzz.rs
+++ examples/fuzz.rs
@@ -16,10 +16,12 @@ fn main() {
             };
 
             println!("n={} r={} k={}", n, r, k);
-            let (a, b, alphabet, stats) = setup_with_seed(n, e, r);
+            let (ref a, ref b, ref alphabet, stats) = setup_with_seed(n, e, r);
             //println!("{}\n{}", to_string(&a), to_string(&b));
             let result = align(&a, &b, &alphabet, stats, h);
-            result.print();
+            let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
+            assert_eq!(result.edit_distance, dist);
+            //result.print();
         }
     }
 }
