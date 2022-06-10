4292ce65acd0ef4876c73cef8caf4d0344f9ad59
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-06-10 14:15:58 -0700

    Tests with/without gapcost

--- tests/contours.rs
+++ tests/contours.rs
@@ -1,7 +1,7 @@
 use pairwise_aligner::prelude::{SH, *};
 
 #[test]
-fn exact_no_pruning() {
+fn exact_no_pruning_gap() {
     for k in [4, 5] {
         for n in [40, 100, 200, 500] {
             for e in [0.1, 0.3, 1.0] {
@@ -26,7 +26,7 @@ fn exact_no_pruning() {
 }
 
 #[test]
-fn inexact_no_pruning() {
+fn inexact_no_pruning_gap() {
     for k in [6, 7] {
         for n in [40, 100, 200, 500] {
             for e in [0.1, 0.3, 1.0] {
@@ -52,7 +52,7 @@ fn inexact_no_pruning() {
 }
 
 #[test]
-fn pruning_bruteforce() {
+fn pruning_bruteforce_gap() {
     for (k, max_match_cost) in [(4, 0), (5, 0), (6, 1), (7, 1)] {
         for n in [40, 100, 200, 500] {
             for e in [0.1, 0.3, 1.0] {
@@ -77,7 +77,7 @@ fn pruning_bruteforce() {
 }
 
 #[test]
-fn pruning_hint_bruteforce() {
+fn pruning_hint_bruteforce_gap() {
     for (k, max_match_cost) in [(4, 0), (5, 0), (6, 1), (7, 1)] {
         for n in [40, 100, 200, 500, 1000] {
             for e in [0.1, 0.3, 1.0] {
@@ -102,7 +102,7 @@ fn pruning_hint_bruteforce() {
 }
 
 #[test]
-fn pruning_hint_central() {
+fn pruning_hint_central_gap() {
     for (k, max_match_cost) in [(4, 0), (5, 0), (6, 1), (7, 1)] {
         for n in [40, 100, 200, 500, 1000] {
             for e in [0.1, 0.3, 1.0] {
@@ -114,6 +114,132 @@ fn pruning_hint_central() {
                     },
                     pruning: true,
                     use_gap_cost: true,
+                    c: PhantomData::<HintContours<CentralContour>>,
+                };
+                let (a, b, alph, stats) = setup(n, e);
+                println!("TESTING n {} e {}: {:?}", n, e, h);
+                let r = align(&a, &b, &alph, stats, h.equal_to_bruteforce_contours());
+                let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
+                assert_eq!(r.edit_distance, dist);
+            }
+        }
+    }
+}
+
+#[test]
+fn exact_no_pruning() {
+    for k in [4, 5] {
+        for n in [40, 100, 200, 500] {
+            for e in [0.1, 0.3, 1.0] {
+                let h = CSH {
+                    match_config: MatchConfig {
+                        length: Fixed(k),
+                        max_match_cost: 0,
+                        ..MatchConfig::default()
+                    },
+                    pruning: false,
+                    use_gap_cost: false,
+                    c: PhantomData::<BruteForceContours>,
+                };
+                let (a, b, alph, stats) = setup(n, e);
+                println!("TESTING n {} e {}: {:?}", n, e, h);
+                let r = align(&a, &b, &alph, stats, h.equal_to_zero_cost_seed_heuristic());
+                let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
+                assert_eq!(r.edit_distance, dist);
+            }
+        }
+    }
+}
+
+#[test]
+fn inexact_no_pruning() {
+    for k in [6, 7] {
+        for n in [40, 100, 200, 500] {
+            for e in [0.1, 0.3, 1.0] {
+                let h = CSH {
+                    match_config: MatchConfig {
+                        length: Fixed(k),
+                        max_match_cost: 1,
+                        ..MatchConfig::default()
+                    },
+                    pruning: false,
+                    use_gap_cost: false,
+                    c: PhantomData::<BruteForceContours>,
+                };
+                let (a, b, alph, stats) = setup(n, e);
+                //print(h, &a, &b, &alph);
+                println!("TESTING n {} e {}: {:?}", n, e, h);
+                let r = align(&a, &b, &alph, stats, h.equal_to_zero_cost_seed_heuristic());
+                let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
+                assert_eq!(r.edit_distance, dist);
+            }
+        }
+    }
+}
+
+#[test]
+fn pruning_bruteforce() {
+    for (k, max_match_cost) in [(4, 0), (5, 0), (6, 1), (7, 1)] {
+        for n in [40, 100, 200, 500] {
+            for e in [0.1, 0.3, 1.0] {
+                let h = CSH {
+                    match_config: MatchConfig {
+                        length: Fixed(k),
+                        max_match_cost,
+                        ..MatchConfig::default()
+                    },
+                    pruning: true,
+                    use_gap_cost: false,
+                    c: PhantomData::<BruteForceContours>,
+                };
+                let (a, b, alph, stats) = setup(n, e);
+                println!("TESTING n {} e {}: {:?}", n, e, h);
+                let r = align(&a, &b, &alph, stats, h.equal_to_zero_cost_seed_heuristic());
+                let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
+                assert_eq!(r.edit_distance, dist);
+            }
+        }
+    }
+}
+
+#[test]
+fn pruning_hint_bruteforce() {
+    for (k, max_match_cost) in [(4, 0), (5, 0), (6, 1), (7, 1)] {
+        for n in [40, 100, 200, 500, 1000] {
+            for e in [0.1, 0.3, 1.0] {
+                let h = CSH {
+                    match_config: MatchConfig {
+                        length: Fixed(k),
+                        max_match_cost,
+                        ..MatchConfig::default()
+                    },
+                    pruning: true,
+                    use_gap_cost: false,
+                    c: PhantomData::<HintContours<BruteForceContour>>,
+                };
+                let (a, b, alph, stats) = setup(n, e);
+                println!("TESTING n {} e {}: {:?}", n, e, h);
+                let r = align(&a, &b, &alph, stats, h.equal_to_bruteforce_contours());
+                let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
+                assert_eq!(r.edit_distance, dist);
+            }
+        }
+    }
+}
+
+#[test]
+fn pruning_hint_central() {
+    for (k, max_match_cost) in [(4, 0), (5, 0), (6, 1), (7, 1)] {
+        for n in [40, 100, 200, 500, 1000] {
+            for e in [0.1, 0.3, 1.0] {
+                let h = CSH {
+                    match_config: MatchConfig {
+                        length: Fixed(k),
+                        max_match_cost,
+                        ..MatchConfig::default()
+                    },
+                    pruning: true,
+                    use_gap_cost: false,
                     c: PhantomData::<HintContours<CentralContour>>,
                 };
                 let (a, b, alph, stats) = setup(n, e);
