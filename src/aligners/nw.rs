95d7e1c277d836a1336d0600c59cf37d3f552a63
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-06-10 17:46:07 -0700

    NW: extract logic to compute next layer into a separate function

--- src/aligners/nw.rs
+++ src/aligners/nw.rs
@@ -6,6 +6,28 @@ struct NW;
 
 const INF: Cost = Cost::MAX;
 
+impl NW {
+    /// Computes the next layer from the current one.
+    /// `ca` is the `i`th character of sequence `a`.
+    fn next_layer(
+        cm: &LinearCost,
+        i: usize,
+        ca: u8,
+        b: &Sequence,
+        prev: &Vec<Cost>,
+        next: &mut Vec<Cost>,
+    ) {
+        next[0] = (i + 1) as Cost * cm.ins();
+        for (j, &cb) in b.iter().enumerate() {
+            next[j + 1] = min(
+                // Convert sub_cost to INF when substitutions are not allowed.
+                prev[j].saturating_add(cm.sub_cost(ca, cb).unwrap_or(INF)),
+                min(next[j] + cm.ins(), prev[j + 1] + cm.del()),
+            );
+        }
+    }
+}
+
 impl Aligner for NW {
     type Params = ();
     type CostModel = LinearCost;
@@ -21,14 +43,7 @@ impl Aligner for NW {
         // TODO: Does enumerate_from exist?
         for (i, &ca) in a.iter().enumerate() {
             swap(&mut next, &mut prev);
-            next[0] = (i + 1) as Cost * cm.ins();
-            for (j, &cb) in b.iter().enumerate() {
-                next[j + 1] = min(
-                    // Convert sub_cost to INF when substitutions are not allowed.
-                    prev[j].saturating_add(cm.sub_cost(ca, cb).unwrap_or(INF)),
-                    min(next[j] + cm.ins(), prev[j + 1] + cm.del()),
-                );
-            }
+            NW::next_layer(cm, i, ca, b, &prev, &mut next);
         }
 
         return next[a.len()];
@@ -49,21 +64,21 @@ impl Aligner for NW {
             visualizer.expand(Pos(0, j as I));
             m[0][j] = j as Cost * cm.ins();
         }
-        for i in 1..=a.len() {
-            visualizer.expand(Pos(i as I, 0));
-            m[i][0] = i as Cost * cm.del();
-        }
         for (i, &ca) in a.iter().enumerate() {
-            let i = i + 1;
-            for (j, &cb) in b.iter().enumerate() {
-                let j = j + 1;
-                visualizer.expand(Pos(i as I, j as I));
-                m[i][j] = min(
-                    // Convert sub_cost to INF when substitutions are not allowed.
-                    m[i - 1][j - 1].saturating_add(cm.sub_cost(ca, cb).unwrap_or(INF)),
-                    min(m[i][j - 1] + cm.ins(), m[i - 1][j] + cm.del()),
-                );
+            for j in 0..=b.len() {
+                visualizer.expand(Pos(i as I + 1, j as I));
             }
+            // We can't pass m[i] and m[i+1] both at the same time, so we must split the vector instead.
+            // TODO: Is there a `get_two` method somewhere?
+            let (front, back) = m.split_at_mut(i + 1);
+            NW::next_layer(
+                cm,
+                i,
+                ca,
+                b,
+                front.last().unwrap(),
+                back.first_mut().unwrap(),
+            );
         }
 
         return m[a.len()][b.len()];
