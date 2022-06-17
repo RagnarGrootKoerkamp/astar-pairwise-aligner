ead0b291875856aa9fbb2b5e6e85736f75234dc9
Mykola Akulov <akulov5120@gmail.com>
2022-06-17 21:10:23 +0300

    Fixed test

--- src/aligners/tests.rs
+++ src/aligners/tests.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use crate::{
     generate::setup,
     prelude::{to_string, AffineCost},
@@ -11,21 +13,17 @@ fn test1() {
     let cm = AffineCost::new_affine(1, 2, 1);
     let tmp: NW<AffineCost<2>> = NW { cm: cm.clone() };
     let tmp2: DiagonalTransition<AffineCost<2>> = DiagonalTransition::new(cm);
-    let start = 1;
-    let stop = 500;
-    let step = 5;
-    let mut len = start;
-    'main: loop {
+    for len in (0..500).step(5) {
         // Test section
-        for k in 0..=5 {
+        for k in [0., 0.01, 0.05, 0.10, 0.20, 0.30, 0.50, 1.0] {
             let (ref a, ref b, ref _alphabet, _stats) = setup(len, 0.2 * k as f32);
-            println!("{len}");
-            print!("s1 == {}\ns2 == {}\n", to_string(&a), to_string(&b));
-            assert_eq!(tmp.align(a, b).0, tmp2.align(a, b).0);
-        }
-        len += step;
-        if len > stop {
-            break 'main;
+            assert_eq!(
+                tmp.align(a, b).0,
+                tmp2.align(a, b).0,
+                "{len}\ns1 == {}\ns2 == {}\n",
+                to_string(&a),
+                to_string(&b)
+            );
         }
     }
 }
