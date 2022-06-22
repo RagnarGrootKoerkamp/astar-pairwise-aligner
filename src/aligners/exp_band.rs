8ee1482c6b7c89f37ff0f34a0072d1d36e3683b0
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-06-22 18:14:40 +0200

    ExpBand: work in progress

--- src/aligners/exp_band.rs
+++ src/aligners/exp_band.rs
@@ -1,11 +1,12 @@
 use super::cigar::Cigar;
-use super::layer::Layers;
+use super::front::Layers;
 use super::nw::{NW, PATH};
 use super::NoVisualizer;
 use super::{Aligner, Visualizer};
 use crate::cost_model::*;
 use crate::prelude::{Pos, Sequence};
-use std::cmp::max;
+use std::cmp::{max, min};
+use std::ops::RangeInclusive;
 
 pub struct Exponential<CostModel> {
     pub cm: CostModel,
@@ -14,12 +15,13 @@ pub struct Exponential<CostModel> {
     pub use_gapcost: bool,
 }
 
-type I = usize;
+/// Type used for indexing sequences.
+type Idx = usize;
 
 // TODO: Instead use saturating add everywhere?
 const INF: Cost = Cost::MAX / 2;
 
-type Front<const N: usize> = super::front::Front<N, Cost, I>;
+type Front<const N: usize> = super::front::Front<N, Cost, Idx>;
 
 /// Settings for the algorithm, and derived constants.
 ///
@@ -28,60 +30,83 @@ type Front<const N: usize> = super::front::Front<N, Cost, I>;
 /// The latter can contain the sequences, direction, and other specifics.
 pub struct ExpBand<CostModel> {
     /// The CostModel to use, possibly affine.
-    cm: CostModel,
+    pub cm: CostModel,
 
     /// When false, the band covers all states with distance <=s.
     /// When true, we only cover states with distance <=s/2.
-    use_gap_cost_heuristic: bool,
+    pub use_gap_cost_heuristic: bool,
 }
 
 impl<const N: usize> ExpBand<AffineCost<N>> {
     /// The first active row in column `i`, when searching up to distance `s`.
-    #[inline]
-    fn jmin(&self, a: &Sequence, b: &Sequence, i: I, s: Cost) -> I {
-        if self.use_gap_cost_heuristic {
-            let d = a.len() as isize - b.len() as isize;
+    fn j_range(&self, a: &Sequence, b: &Sequence, i: Idx, s: Cost) -> RangeInclusive<Idx> {
+        let start_j = if self.use_gap_cost_heuristic {
+            let d = a.len() as Idx - b.len() as Idx;
             // FIXME: Get the rounding right here.
-            i - ((s - d as Cost * self.cm.min_ins_extend)
-                / (self.cm.min_del_extend + self.cm.min_ins_extend)) as I
+            i - ((s as Idx - d as Idx * self.cm.min_ins_extend as Idx)
+                / (self.cm.min_del_extend + self.cm.min_ins_extend) as Idx)
         } else {
-            i.saturating_sub((s / self.cm.min_ins_extend) as I)
-        }
-    }
-    /// The last active column for the given front.
-    #[inline]
-    fn jmax(&self, a: &Sequence, b: &Sequence, i: I, s: Cost) -> I {
-        if self.use_gap_cost_heuristic {
-            let d = a.len() as isize - b.len() as isize;
+            i.saturating_sub((s / self.cm.min_ins_extend) as Idx)
+        };
+        let end_j = if self.use_gap_cost_heuristic {
+            let d = a.len() as Idx - b.len() as Idx;
             // FIXME: Get the rounding right here.
-            i + ((s + d as Cost * self.cm.min_del_extend)
-                / (self.cm.min_del_extend + self.cm.min_ins_extend)) as I
+            i + (s as Idx + d * self.cm.min_del_extend as Idx)
+                / (self.cm.min_del_extend + self.cm.min_ins_extend) as Idx
         } else {
-            i + (s / self.cm.min_del_extend) as I
-        }
+            i + (s / self.cm.min_del_extend) as Idx
+        };
+        max(start_j, 0)..=min(end_j, b.len())
     }
 }
 
 impl<const N: usize> ExpBand<AffineCost<N>> {
     /// Test whether the cost is at most s.
-    /// Returns None or cost > s, or the actual cost otherwise.
+    /// Returns None if cost > s, or the actual cost otherwise.
     fn cost_for_band(&self, a: &Sequence, b: &Sequence, s: Cost) -> Option<Cost> {
-        let range = self.jmin(a, b, 0, s)..=self.jmax(a, b, 0, s);
+        let range = self.j_range(a, b, 0, s);
+        let right_buffer = 2;
         let ref mut prev = Front {
-            layers: Layers::new(vec![INF; range.end() - range.start() + 1]),
+            layers: Layers::new(vec![INF; range.end() - range.start() + 1 + right_buffer]),
+            offset: *range.start(),
             range,
-            offset: self.jmin(a, b, 0, s),
         };
         let ref mut next = prev.clone();
-        next.m()[0] = 0;
+
+        // TODO: Find a way to not have to manually process the first layer.
+        // TODO: Reuse from NW.
+        next.m_mut()[0] = 0;
+        for j in next.range.clone() {
+            // Initialize the main layer with linear insertions.
+            next.m_mut()[j] = self.cm.ins_or(INF, |ins| j as Cost * ins);
+
+            // Initialize the affine insertion layers.
+            for (layer_idx, cm) in self.cm.affine.iter().enumerate() {
+                let (mut next_m, mut next_layer) = next.m_affine_mut(layer_idx);
+                match cm.affine_type {
+                    DeleteLayer => {}
+                    InsertLayer => {
+                        next_layer[j] = cm.open + j as Cost * cm.extend;
+                    }
+                    _ => todo!(),
+                };
+                next_m[j] = min(next_m[j], next_layer[j]);
+            }
+        }
 
         for (i0, &ca) in a.iter().enumerate() {
             // Convert to 1 based index.
             let i = i0 + 1;
             std::mem::swap(prev, next);
             // Update front parameters.
-            next.range = self.jmin(a, b, i, s)..=self.jmax(a, b, i, s);
-            NW { cm: self.cm }.next_front(i, ca, b, prev, next, &mut NoVisualizer);
+            next.range = self.j_range(a, b, i, s);
+            // FIXME: Should be negative?
+            next.offset = -*next.range.start();
+            // FIXME: Take a ref instead of clone.
+            NW {
+                cm: self.cm.clone(),
+            }
+            .next_front(i, ca, b, prev, next, &mut NoVisualizer);
         }
 
         if let Some(&dist) = next.m().get(b.len()) {
@@ -91,22 +116,99 @@ impl<const N: usize> ExpBand<AffineCost<N>> {
         }
         None
     }
+
+    /// Tries to find a path with cost <= s.
+    /// Returns None if cost > s, or the actual cost otherwise.
+    fn path_for_band(
+        &self,
+        a: &Sequence,
+        b: &Sequence,
+        s: Cost,
+        v: &mut impl Visualizer,
+    ) -> Option<(Cost, PATH, Cigar)> {
+        let range = self.j_range(a, b, 0, s);
+        let ref mut fronts = vec![
+            Front {
+                layers: Layers::new(vec![INF; range.end() - range.start() + 1]),
+                offset: *range.start(),
+                range,
+            };
+            a.len() + 1
+        ];
+
+        // TODO: Find a way to not have to manually process the first layer.
+        v.expand(Pos(0, 0));
+        fronts[0].m_mut()[0] = 0;
+        for j in 1..=b.len() {
+            v.expand(Pos(0, j as crate::prelude::I));
+            // Initialize the main layer with linear deletions.
+            fronts[0].m_mut()[j] = self.cm.del_or(INF, |del| j as Cost * del);
+
+            // Initialize the affine deletion layers.
+            for (layer_idx, cm) in self.cm.affine.iter().enumerate() {
+                let (mut next_m, mut next_layer) = fronts[0].m_affine_mut(layer_idx);
+                match cm.affine_type {
+                    DeleteLayer => {
+                        next_layer[j] = cm.open + j as Cost * cm.extend;
+                    }
+                    InsertLayer => {}
+                    _ => todo!(),
+                };
+                next_m[j] = min(next_m[j], next_layer[j]);
+            }
+        }
+
+        for (i0, &ca) in a.iter().enumerate() {
+            // Convert to 1 based index.
+            let i = i0 + 1;
+            let [prev, next] = &mut fronts[i-1..=i] else {unreachable!();};
+            // Update front parameters.
+            next.range = self.j_range(a, b, i, s);
+            next.offset = *next.range.start();
+            // FIXME: Take a ref instead of clone.
+            NW {
+                cm: self.cm.clone(),
+            }
+            .next_front(i, ca, b, prev, next, v);
+        }
+
+        if let Some(&dist) = fronts[a.len()].m().get(b.len()) {
+            if dist <= s {
+                let (path, cigar) = NW {
+                    cm: self.cm.clone(),
+                }
+                .track_path(fronts, a, b);
+                return Some((dist, path, cigar));
+            }
+        }
+        None
+    }
+
+    fn exponential_search_s<T>(
+        &self,
+        a: &Sequence,
+        b: &Sequence,
+        mut f: impl FnMut(Cost) -> Option<T>,
+    ) -> T {
+        // Really, when self.gap_heuristic is false we should start at 0 instead of the difference.
+        let mut s = if a.len() >= b.len() {
+            (a.len() - b.len()) as Cost * self.cm.min_del_extend
+        } else {
+            (b.len() - a.len()) as Cost * self.cm.min_ins_extend
+        };
+        // TODO: Fix the potential infinite loop here.
+        loop {
+            if let Some(d) = f(s) {
+                return d;
+            }
+            s = max(2 * s, 1);
+        }
+    }
 }
 
 impl<const N: usize> Aligner for ExpBand<AffineCost<N>> {
     fn cost(&self, a: &Sequence, b: &Sequence) -> Cost {
-        // Really, when self.gap_heuristic is false we should start at 0 instead of the difference.
-        let mut s = if a.len() >= b.len() {
-            (a.len() - b.len()) as Cost / self.cm.min_del_extend
-        } else {
-            (b.len() - a.len()) as Cost / self.cm.min_ins_extend
-        };
-        loop {
-            if let Some(d) = self.cost_for_band(a, b, s) {
-                break d;
-            }
-            s = max(2 * s, 1);
-        }
+        self.exponential_search_s(a, b, |s| self.cost_for_band(a, b, s))
     }
 
     fn visualize(
@@ -115,29 +217,6 @@ impl<const N: usize> Aligner for ExpBand<AffineCost<N>> {
         b: &Sequence,
         v: &mut impl Visualizer,
     ) -> (Cost, PATH, Cigar) {
-        let Some(ref mut fronts) = self.init_fronts(a, b, v) else {
-            return (0,vec![],Cigar::default());
-        };
-
-        v.expand(Pos(0, 0));
-
-        let mut s = 0;
-        loop {
-            s += 1;
-
-            // A temporary front without any content.
-            let mut next = Front::<N> {
-                layers: Layers::<N, Vec<Fr>>::new(vec![]),
-                range: self.jmin(s)..=self.jmax(s),
-                offset: self.left_buffer as Fr - self.jmin(s),
-            };
-
-            if self.next_front(a, b, fronts, &mut next, v) {
-                // FIXME: Reconstruct path.
-                return (s, vec![], Cigar::default());
-            }
-
-            fronts.push(next);
-        }
+        self.exponential_search_s(a, b, |s| self.path_for_band(a, b, s, v))
     }
 }
