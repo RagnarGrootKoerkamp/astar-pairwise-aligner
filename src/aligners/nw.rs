ceeb4699547860e6c81656f22b732f14d6c5fcb5
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-06-26 11:15:49 +0200

    NW + EXPONENTIAL_SEARCH + HEURISTIC

--- src/aligners/nw.rs
+++ src/aligners/nw.rs
@@ -1,22 +1,29 @@
-use itertools::chain;
-
 use super::cigar::Cigar;
 use super::edit_graph::{EditGraph, State};
-use super::{Aligner, VisualizerT};
+use super::{Aligner, NoVisualizer, VisualizerT};
 use super::{Seq, Sequence};
 use crate::cost_model::*;
+use crate::heuristic::{Heuristic, HeuristicInstance, ZeroCost};
 use crate::prelude::Pos;
+use itertools::chain;
 use std::cmp::{max, min};
 use std::ops::RangeInclusive;
 
 pub type Path = Vec<Pos>;
-pub struct NW<CostModel, V: VisualizerT> {
+
+/// Needleman-Wunsch aligner.
+///
+/// NOTE: Heuristics only support unit cost graph for now.
+pub struct NW<CostModel, V: VisualizerT, H: Heuristic> {
     /// The cost model to use.
     pub cm: CostModel,
 
     /// When false, the band covers all states with distance <=s.
     /// When true, we only cover states with distance <=s/2.
     pub use_gap_cost_heuristic: bool,
+
+    /// The heuristic to use.
+    pub h: H,
 
     /// The visualizer to use.
     pub v: V,
@@ -36,10 +43,19 @@ type Fronts<const N: usize> = super::front::Fronts<N, Cost, Idx>;
 /// NW DP only needs the cell just left and above of the current cell.
 const LEFT_BUFFER: Idx = 1;
 const RIGHT_BUFFER: Idx = 1;
-/// After padding `a` and `b` at the front, an extra buffer isn't needed anymore.
-const TOP_BUFFER: Idx = 0;
 
-impl<const N: usize, V: VisualizerT> NW<AffineCost<N>, V> {
+impl<const N: usize> NW<AffineCost<N>, NoVisualizer, ZeroCost> {
+    pub fn new(cm: AffineCost<N>, use_gap_cost_heuristic: bool) -> Self {
+        Self {
+            cm,
+            use_gap_cost_heuristic,
+            h: ZeroCost,
+            v: NoVisualizer,
+        }
+    }
+}
+
+impl<const N: usize, V: VisualizerT, H: Heuristic> NW<AffineCost<N>, V, H> {
     fn track_path(&self, fronts: &Fronts<N>, a: Seq, b: Seq) -> (Path, Cigar) {
         let mut path: Path = vec![];
         let mut cigar = Cigar::default();
@@ -71,7 +87,10 @@ impl<const N: usize, V: VisualizerT> NW<AffineCost<N>, V> {
                 st,
                 |di, dj, new_layer, cost, ops| {
                     if parent.is_none()
-                        && cur_cost == fronts[st.i + di].layer(new_layer)[st.j + dj] + cost
+                        // We use `get` to handle possible out-of-bound lookups.
+                        && let Some(parent_cost) =
+                            fronts[st.i + di].layer(new_layer).get(st.j + dj)
+                        && cur_cost == parent_cost + cost
                     {
                         parent = Some(State::new(st.i + di, st.j + dj, new_layer));
                         save(st);
@@ -112,14 +131,25 @@ impl<const N: usize, V: VisualizerT> NW<AffineCost<N>, V> {
                     /*greedy_matching=*/ false,
                     State::new(i, j, layer),
                     |di, dj, layer, edge_cost, _cigar_ops| {
-                        best = min(
-                            best,
-                            if di == 0 {
-                                next.layer(layer)[j + dj] + edge_cost
+                        if H::IS_DEFAULT {
+                            best = min(
+                                best,
+                                if di == 0 {
+                                    next.layer(layer)[j + dj] + edge_cost
+                                } else {
+                                    prev.layer(layer)[j + dj] + edge_cost
+                                },
+                            );
+                        } else {
+                            let parent_cost = if di == 0 {
+                                next.layer(layer).get(j + dj)
                             } else {
-                                prev.layer(layer)[j + dj] + edge_cost
-                            },
-                        );
+                                prev.layer(layer).get(j + dj)
+                            };
+                            if let Some(cost) = parent_cost {
+                                best = min(best, cost + edge_cost);
+                            }
+                        }
                     },
                 );
                 next.layer_mut(layer)[j] = best;
@@ -128,32 +158,94 @@ impl<const N: usize, V: VisualizerT> NW<AffineCost<N>, V> {
     }
 
     /// The range of rows `j` to consider in column `i`, when the cost is bounded by `s_bound`.
-    fn j_range(&self, a: Seq, b: Seq, i: Idx, s_bound: Option<Cost>) -> RangeInclusive<Idx> {
+    fn j_range(
+        &self,
+        a: Seq,
+        b: Seq,
+        h: &H::Instance<'_>,
+        i: Idx,
+        s_bound: Option<Cost>,
+        prev: &Front<N>,
+    ) -> RangeInclusive<Idx> {
+        // Without a bound on the distance, we can notuse any heuristic.
         let Some(mut s) = s_bound else {
             return 1..=b.len() as Idx;
         };
-        let range = if self.use_gap_cost_heuristic {
-            let d = b.len() as Idx - a.len() as Idx;
-            // We subtract the cost needed to bridge the gap from the start to the end.
-            s -= self.cm.gap_cost(Pos(0, 0), Pos::from_lengths(a, b));
-            // Each extra diagonal costs one insertion and one deletion.
-            let extra_diagonals = s / (self.cm.min_ins_extend + self.cm.min_del_extend);
-            // NOTE: The range could be reduced slightly further by considering gap open costs.
-            min(d, 0) - extra_diagonals as Idx..=max(d, 0) + extra_diagonals as Idx
+        if H::IS_DEFAULT {
+            // For the default heuristic, either use the full range of diagonals
+            // covered by distance `s`, or do only the gap-cost to the end when
+            // needed.
+            let range = if self.use_gap_cost_heuristic {
+                let d = b.len() as Idx - a.len() as Idx;
+                // We subtract the cost needed to bridge the gap from the start to the end.
+                s -= self.cm.gap_cost(Pos(0, 0), Pos::from_lengths(a, b));
+                // Each extra diagonal costs one insertion and one deletion.
+                let extra_diagonals = s / (self.cm.min_ins_extend + self.cm.min_del_extend);
+                // NOTE: The range could be reduced slightly further by considering gap open costs.
+                min(d, 0) - extra_diagonals as Idx..=max(d, 0) + extra_diagonals as Idx
+            } else {
+                -(self.cm.max_del_for_cost(s) as Idx)..=self.cm.max_ins_for_cost(s) as Idx
+            };
+            // crop
+            max(i + *range.start(), 1)..=min(i + *range.end(), b.len() as Idx)
         } else {
-            -(self.cm.max_del_for_cost(s) as Idx)..=self.cm.max_ins_for_cost(s) as Idx
-        };
+            if i == 0 {
+                0..=0
+            } else {
+                // Start with the range of the previous front.
+                // Then:
+                // Keep increasing the start while prev[start]+h() > s_bound.
+                // Keep decreasing the end while prev[end]+h() > s_bound.
+                // Keep increasing the end while prev[prev_end]+extend_cost*(end-prev_end)+h() > s_bound.
+                let mut start = *prev.range().start();
 
-        // crop
-        max(i + *range.start(), 1)..=min(i + *range.end(), b.len() as Idx)
+                // To fix the padded character, we do max(start, 1) and max(i,1).
+                // TODO: include the cost needed to transition from column `prev`/`i-1` to the current column.
+                // h.h has (-1, -1) to offset the padding.
+                while start < b.len() as Idx
+                    && start <= prev.range().end() + 1
+                    && prev.m()[start] + h.h(Pos::from(max(i, 1) - 1, max(start, 1) - 1)) > s
+                {
+                    start += 1;
+                }
+                start = max(start, 1);
+                if start > prev.range().end() + 1 {
+                    return start..=start - 1;
+                }
+                let prev_end = *prev.range().end();
+                let prev_end_cost = prev.m()[prev_end];
+                let mut end = prev_end;
+
+                // Decrease end as needed.
+                while end >= start && prev.m()[end] + h.h(Pos::from(max(i, 1) - 1, end - 1)) > s {
+                    end -= 1;
+                }
+
+                // Increase end as needed.
+
+                // We use the cheapest possible way to extend vertically.
+                // h.h has (-1, -1) to offset the padding.
+                while end < b.len() as Idx
+                    && prev_end_cost
+                        + self
+                            .cm
+                            .extend_cost(Pos::from(i - 1, prev_end), Pos::from(i, end + 1))
+                        + h.h(Pos::from(i - 1, end + 1 - 1))
+                        <= s
+                {
+                    end += 1;
+                }
+                start..=end
+            }
+        }
     }
 }
 
 fn pad(a: Seq) -> Sequence {
-    chain!(b"^", a, b"$").copied().collect()
+    chain!(b"^", a).copied().collect()
 }
 
-impl<const N: usize, V: VisualizerT> Aligner for NW<AffineCost<N>, V> {
+impl<const N: usize, V: VisualizerT, H: Heuristic> Aligner for NW<AffineCost<N>, V, H> {
     type CostModel = AffineCost<N>;
 
     fn cost_model(&self) -> &Self::CostModel {
@@ -167,10 +259,16 @@ impl<const N: usize, V: VisualizerT> Aligner for NW<AffineCost<N>, V> {
         let ref a = pad(a);
         let ref b = pad(b);
 
+        let ref mut h = self.h.build(
+            &a[1..a.len()],
+            &b[1..b.len()],
+            &bio::alphabets::dna::alphabet(),
+        );
+
         let ref mut prev = Front::default();
         let ref mut next = Front::new(
             INF,
-            self.j_range(a, b, 0, s_bound),
+            self.j_range(a, b, h, 0, s_bound, prev),
             LEFT_BUFFER,
             RIGHT_BUFFER,
         );
@@ -180,7 +278,7 @@ impl<const N: usize, V: VisualizerT> Aligner for NW<AffineCost<N>, V> {
             // Update front size.
             next.reset(
                 INF,
-                self.j_range(a, b, i, s_bound),
+                self.j_range(a, b, h, i, s_bound, prev),
                 LEFT_BUFFER,
                 RIGHT_BUFFER,
             );
@@ -206,23 +304,36 @@ impl<const N: usize, V: VisualizerT> Aligner for NW<AffineCost<N>, V> {
         let ref a = pad(a);
         let ref b = pad(b);
 
+        // Build `h` for the original, unpadded strings.
+        let ref mut h = self.h.build(
+            &a[1..a.len()],
+            &b[1..b.len()],
+            &bio::alphabets::dna::alphabet(),
+        );
+
         let mut fronts = Fronts::new(
             INF,
             // The fronts to create.
-            0..=a.len() as Idx,
+            0..=0 as Idx,
             // The range for each front.
-            |i| self.j_range(a, b, i, s_bound),
-            TOP_BUFFER,
+            |i| self.j_range(a, b, h, i, s_bound, &Front::default()),
+            0,
             0,
             LEFT_BUFFER,
             RIGHT_BUFFER,
         );
         fronts[0].m_mut()[0] = 0;
 
-        for i in 1..=a.len() {
-            let i = i as Idx;
-            let [prev, next] = &mut fronts[i-1..=i] else {unreachable!();};
-            self.next_front(i, a, b, prev, next);
+        for i in 1..=a.len() as Idx {
+            let prev = &fronts[i - 1];
+            let mut next = Front::new(
+                INF,
+                self.j_range(a, b, h, i, s_bound, prev),
+                LEFT_BUFFER,
+                RIGHT_BUFFER,
+            );
+            self.next_front(i, a, b, prev, &mut next);
+            fronts.fronts.push(next);
         }
 
         if let Some(&dist) = fronts[a.len() as Idx].m().get(b.len() as Idx) {
