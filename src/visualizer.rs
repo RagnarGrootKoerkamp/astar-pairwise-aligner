fbff056e332ceef939ab53a475f857f760796590
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-06-29 14:05:50 +0200

    Visualizer: Much cleanup and refactoring

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -1,4 +1,8 @@
-use std::{cell::Cell, ops::Range, time::Duration};
+use std::{
+    ops::Range,
+    path::{Path, PathBuf},
+    time::{Duration, Instant},
+};
 
 use sdl2::{
     event::Event,
@@ -10,14 +14,14 @@ use sdl2::{
     Sdl,
 };
 
-use crate::prelude::Pos;
+use crate::prelude::{Pos, Seq};
 
 /// A visualizer can be used to visualize progress of an implementation.
 pub trait VisualizerT {
     fn explore(&mut self, _pos: Pos) {}
     fn expand(&mut self, _pos: Pos) {}
 
-    //This function may be called after the main loop to display final image with big delay
+    //This function may be called after the main loop to display final image.
     fn last_frame(&mut self) {}
 }
 
@@ -33,21 +37,84 @@ pub enum Gradient {
     TurboGradient(Range<f32>),
 }
 
+impl Gradient {
+    fn expand(&self, f: f32) -> Color {
+        match self {
+            Gradient::NoGradient { expand, .. } => *expand,
+            Gradient::Gradient(range) => {
+                let frac = |a: u8, b: u8| -> u8 { ((1. - f) * a as f32 + f * b as f32) as u8 };
+                Color::RGB(
+                    frac(range.start.r, range.end.r),
+                    frac(range.start.g, range.end.g),
+                    frac(range.start.b, range.end.b),
+                )
+            }
+            Gradient::TurboGradient(range) => {
+                let f = range.start + f * (range.end - range.start);
+                let c = colorgrad::turbo().at(f as f64).rgba_u8();
+                Color::RGBA(c.0, c.1, c.2, c.3)
+            }
+        }
+    }
+
+    fn explore(&self, _f: f32) -> Color {
+        match self {
+            Gradient::NoGradient { explore, .. } => *explore,
+            Gradient::Gradient(_) => todo!(),
+            Gradient::TurboGradient(_) => todo!(),
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct ColorScheme {
-    gradient: Gradient,
-    bg_color: Color,
+    pub gradient: Gradient,
+    pub bg_color: Color,
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum Save {
+    None,
+    Last,
+    All,
+}
+
+impl Save {
+    fn do_save(&self, is_last: bool) -> bool {
+        match &self {
+            Save::None => false,
+            Save::Last => is_last,
+            Save::All => true,
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum Draw {
+    None,
+    Last,
+    All,
+}
+
+impl Draw {
+    fn do_draw(&self, is_last: bool) -> bool {
+        match &self {
+            Draw::None => false,
+            Draw::Last => is_last,
+            Draw::All => true,
+        }
+    }
 }
 
 #[derive(Clone)]
 pub struct Config {
-    cell_size: usize,
-    prescaler: usize, //for scaling image
-    filepath: String, //maybe &str instead
-    drawing: bool,
-    delay: Cell<f32>,
-    saving: bool,
-    colors: ColorScheme,
+    pub cell_size: usize,
+    pub prescaler: usize, //for scaling image
+    pub filepath: String, //maybe &str instead
+    pub draw: Draw,
+    pub delay: f32,
+    pub save: Save,
+    pub colors: ColorScheme,
 }
 
 impl Default for Config {
@@ -55,10 +122,10 @@ impl Default for Config {
         Self {
             cell_size: 8,
             prescaler: 1,
-            saving: false,
+            save: Save::None,
             filepath: String::from(""),
-            drawing: false,
-            delay: Cell::new(0.2),
+            draw: Draw::None,
+            delay: 0.2,
             colors: ColorScheme {
                 gradient: Gradient::NoGradient {
                     expand: Color::BLUE,
@@ -70,31 +137,7 @@ impl Default for Config {
     }
 }
 
-//Saves canvas to bmp file
-pub fn save_canvas(
-    canvas: &sdl2::render::Canvas<sdl2::video::Window>,
-    filepath: &str,
-    number: usize,
-) {
-    let pixel_format = canvas.default_pixel_format();
-    let mut pixels = canvas.read_pixels(canvas.viewport(), pixel_format).unwrap();
-    let (width, height) = canvas.output_size().unwrap();
-    let pitch = pixel_format.byte_size_of_pixels(width as usize);
-    let surf = sdl2::surface::Surface::from_data(
-        pixels.as_mut_slice(),
-        width,
-        height,
-        pitch as u32,
-        pixel_format,
-    )
-    .unwrap();
-    surf.save_bmp(format!("{}{}.bmp", filepath, number))
-        .unwrap_or_else(|error| {
-            print!("Problem saving the file: {:?}", error);
-        });
-}
-
-struct Visualizer {
+pub struct Visualizer {
     canvas: Option<sdl2::render::Canvas<sdl2::video::Window>>,
     sdl_context: Sdl,
     config: Config,
@@ -103,51 +146,93 @@ struct Visualizer {
     width: u32,
     height: u32,
     file_number: usize,
-    skip: usize,
 }
 
 impl Visualizer {
-    fn init(&mut self, config: &Config, len1: u32, len2: u32) {
-        self.skip = 0;
-        self.file_number = 0;
-        self.width = len1;
-        self.height = len2;
-        self.config = config.clone();
-        self.sdl_context = sdl2::init().unwrap();
-        let canvas_size_cells = Pos(len1 + 1, len2 + 1);
-        let video_subsystem = self.sdl_context.video().unwrap();
-        video_subsystem.gl_attr().set_double_buffer(true);
-        let window = if config.drawing || config.saving {
-            Some(
-                video_subsystem
-                    .window(
-                        "A*PA",
-                        canvas_size_cells.0 as u32
-                            * config.cell_size as u32
-                            * config.prescaler as u32,
-                        (canvas_size_cells.1 as u32)
-                            * config.cell_size as u32
-                            * config.prescaler as u32,
+    pub fn new(config: Config, a: Seq, b: Seq) -> Self {
+        let sdl_context = sdl2::init().unwrap();
+        Visualizer {
+            canvas: {
+                let canvas_size_cells = Pos::from(a.len() + 1, b.len() + 1);
+                let video_subsystem = sdl_context.video().unwrap();
+                video_subsystem.gl_attr().set_double_buffer(true);
+                if config.draw != Draw::None || config.save != Save::None {
+                    Some(
+                        video_subsystem
+                            .window(
+                                "A*PA",
+                                canvas_size_cells.0 as u32
+                                    * config.cell_size as u32
+                                    * config.prescaler as u32,
+                                (canvas_size_cells.1 as u32)
+                                    * config.cell_size as u32
+                                    * config.prescaler as u32,
+                            )
+                            //.borderless()
+                            .build()
+                            .unwrap()
+                            .into_canvas()
+                            .build()
+                            .unwrap(),
                     )
-                    //.borderless()
-                    .build()
-                    .unwrap(),
-            )
+                } else {
+                    None
+                }
+            },
+            sdl_context,
+            config: config.clone(),
+            expanded: vec![],
+            explored: vec![],
+            width: a.len() as u32 + 1,
+            height: b.len() as u32 + 1,
+            file_number: 0,
+        }
+    }
+
+    //Saves canvas to bmp file
+    pub fn save_canvas(&self, last: bool) {
+        let path = if last {
+            let file = Path::new(&self.config.filepath);
+            if let Some(parent) = file.parent() {
+                std::fs::create_dir_all(parent).unwrap();
+            }
+            file.with_extension("bmp").to_owned()
         } else {
-            None
+            // Make sure the directory exists.
+            let mut dir = PathBuf::from(&self.config.filepath);
+            std::fs::create_dir_all(&dir).unwrap();
+            dir.push(self.file_number.to_string());
+            dir.set_extension("bmp");
+            dir
         };
-        self.canvas = window.map(|w| w.into_canvas().build().unwrap());
+
+        let canvas = self.canvas.as_ref().unwrap();
+
+        let pixel_format = canvas.default_pixel_format();
+        let mut pixels = canvas.read_pixels(canvas.viewport(), pixel_format).unwrap();
+        let (width, height) = canvas.output_size().unwrap();
+        let pitch = pixel_format.byte_size_of_pixels(width as usize);
+        let mut surf = sdl2::surface::Surface::from_data(
+            pixels.as_mut_slice(),
+            width,
+            height,
+            pitch as u32,
+            pixel_format,
+        )
+        .unwrap();
+        surf.set_color_key(true, self.config.colors.bg_color)
+            .unwrap();
+
+        surf.save_bmp(path).unwrap_or_else(|error| {
+            print!("Problem saving the file: {:?}", error);
+        });
     }
 
-    fn last_frame(&mut self) {
-        self.skip = 2;
-        self.draw();
-    }
-
-    fn draw(&mut self) {
-        if !self.config.saving && !self.config.drawing {
+    fn draw(&mut self, is_last: bool) {
+        if !self.config.draw.do_draw(is_last) && !self.config.save.do_save(is_last) {
             return;
         }
+
         let scale = self.config.prescaler as u32;
         let cell_size = self.config.cell_size as u32;
 
@@ -169,138 +254,127 @@ impl Visualizer {
                 .unwrap();
         };
 
-        if let Some(canvas) = &mut self.canvas {
-            canvas.set_draw_color(self.config.colors.bg_color);
-            canvas
-                .fill_rect(Rect::new(0, 0, self.width, self.height))
-                .unwrap();
-            match &self.config.colors.gradient {
-                Gradient::NoGradient { expand, explore } => {
-                    for pos in &self.explored {
-                        draw_pixel(canvas, *pos, *explore);
-                    }
-                    for pos in &self.expanded {
-                        draw_pixel(canvas, *pos, *expand);
-                    }
-                }
-                Gradient::Gradient(range) => {
-                    //Draws only expnded states!
-                    fn gradient(f: f32, c1: Color, c2: Color) -> Color {
-                        let frac =
-                            |a: u8, b: u8| -> u8 { ((1. - f) * a as f32 + f * b as f32) as u8 };
+        let Some(canvas) = &mut self.canvas else {return;};
 
-                        return Color::RGB(frac(c1.r, c2.r), frac(c1.g, c2.g), frac(c1.b, c2.b));
-                    }
-                    let mut i: f32 = 0.;
-                    let d = self.expanded.len() as f32;
-                    for pos in &self.expanded {
-                        draw_pixel(canvas, *pos, gradient(i / d, range.start, range.end));
-                        i += 1.;
-                    }
-                }
-                Gradient::TurboGradient(range) => {
-                    //Draws only expnded states!
-                    let grad = colorgrad::turbo();
-                    let mut i: f64 = 0.;
-                    let d = self.expanded.len() as f64;
-                    let coef = (range.end - range.start) as f64;
-                    for pos in &self.expanded {
-                        let val = i / d;
-                        let clr = grad.at(range.start as f64 + (val * coef)).rgba_u8();
-                        draw_pixel(canvas, *pos, Color::RGBA(clr.0, clr.1, clr.2, clr.3));
-                        i += 1.;
-                    }
-                }
-            }
-            if self.config.saving {
-                save_canvas(&canvas, &self.config.filepath, self.file_number);
+        // Draw background.
+        canvas.set_draw_color(self.config.colors.bg_color);
+        canvas
+            .fill_rect(Rect::new(
+                0,
+                0,
+                cell_size * self.width,
+                cell_size * self.height,
+            ))
+            .unwrap();
+
+        for (i, pos) in self.explored.iter().enumerate() {
+            draw_pixel(
+                canvas,
+                *pos,
+                self.config
+                    .colors
+                    .gradient
+                    .explore(i as f32 / self.explored.len() as f32),
+            );
+        }
+        for (i, pos) in self.expanded.iter().enumerate() {
+            draw_pixel(
+                canvas,
+                *pos,
+                self.config
+                    .colors
+                    .gradient
+                    .expand(i as f32 / self.expanded.len() as f32),
+            );
+        }
+
+        // SAVE
+
+        if self.config.save.do_save(is_last) {
+            if is_last {
+                self.save_canvas(is_last);
+            } else {
+                self.save_canvas(is_last);
                 self.file_number += 1;
             }
-            if self.config.drawing {
-                ::std::thread::sleep(Duration::from_secs_f32(self.config.delay.get()));
-            }
+        }
 
-            //Keyboard events
+        // DRAW
 
-            let sleep_duration = 0.01;
-            let mut duration: f32 = 0.;
-            let mut delay_tmp = self.config.delay.get();
-            let mut is_playing: bool = true;
-            if self.config.drawing {
-                if self.skip == 1 {
-                    return;
-                } else if self.skip == 2 {
-                    delay_tmp = 1000.;
-                }
-                canvas.present();
-                'outer: loop {
-                    for event in self.sdl_context.event_pump().unwrap().poll_iter() {
-                        match event {
-                            Event::Quit { .. }
-                            | Event::KeyDown {
-                                keycode: Some(Keycode::Q),
-                                ..
-                            } => {
-                                panic!();
-                            }
-                            Event::KeyDown {
-                                keycode: Some(key), ..
-                            } => match key {
-                                Keycode::P => {
-                                    //pause
-                                    is_playing = !is_playing;
-                                }
-                                Keycode::Escape => {
-                                    //next frame
-                                    break 'outer;
-                                }
-                                Keycode::F => {
-                                    //faster
-                                    delay_tmp *= 0.8;
-                                }
-                                Keycode::S => {
-                                    //slower
-                                    delay_tmp /= 0.8;
-                                }
-                                Keycode::A => {
-                                    //skip to the last frame
-                                    if self.skip == 0 {
-                                        self.skip = 1;
-                                    }
-                                    break 'outer;
-                                }
-                                _ => {}
-                            },
-                            _ => {}
-                        }
+        if !self.config.draw.do_draw(is_last) {
+            return;
+        }
+
+        //Keyboard events
+
+        let sleep_duration = 0.00001;
+        let mut paused = false;
+        self.canvas.as_mut().unwrap().present();
+        let mut start_time = Instant::now();
+        'outer: loop {
+            for event in self.sdl_context.event_pump().unwrap().poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::X),
+                        ..
+                    } => {
+                        panic!("Running aborted by user!");
                     }
-                    ::std::thread::sleep(Duration::from_secs_f32(sleep_duration));
-                    if is_playing {
-                        duration += sleep_duration;
-                        if duration >= delay_tmp {
+                    Event::KeyDown {
+                        keycode: Some(key), ..
+                    } => match key {
+                        Keycode::P => {
+                            //pause
+                            if paused {
+                                paused = false;
+                                start_time = Instant::now();
+                            } else {
+                                paused = true;
+                            }
+                        }
+                        Keycode::Escape | Keycode::Space => {
+                            //next frame
                             break 'outer;
                         }
-                    }
+                        Keycode::F => {
+                            //faster
+                            self.config.delay *= 0.8;
+                        }
+                        Keycode::S => {
+                            //slower
+                            self.config.delay /= 0.8;
+                        }
+                        Keycode::Q => {
+                            self.config.draw = Draw::Last;
+                            break 'outer;
+                        }
+                        _ => {}
+                    },
+                    _ => {}
                 }
             }
-            if self.skip == 2 {
-                self.skip = 0;
-            } else {
-                self.config.delay.set(delay_tmp);
+            ::std::thread::sleep(Duration::from_secs_f32(sleep_duration));
+
+            if !paused && !is_last && start_time.elapsed().as_secs_f32() >= self.config.delay {
+                break 'outer;
             }
-            return;
         }
     }
 }
 
 impl VisualizerT for Visualizer {
-    fn expand(&mut self, _pos: Pos) {
-        self.expanded.push(_pos);
-        self.draw();
+    fn expand(&mut self, pos: Pos) {
+        self.expanded.push(pos);
+        self.draw(false);
     }
 
-    fn explore(&mut self, _pos: Pos) {
-        self.explored.push(_pos);
-        self.draw();
+    fn explore(&mut self, pos: Pos) {
+        self.explored.push(pos);
+        self.draw(false);
+    }
+
+    fn last_frame(&mut self) {
+        self.draw(true);
     }
 }
