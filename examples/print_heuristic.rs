e6c306f8c8f038cf714c648f535b723ddaa5e65e
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-07-11 11:13:47 +0200

    Shrink bad testcase

--- examples/print_heuristic.rs
+++ examples/print_heuristic.rs
@@ -1,21 +1,21 @@
 use pairwise_aligner::{prelude::*, visualizer::NoVisualizer};
 
 fn main() {
-    let pruning = false;
-    let n = 11;
-    let e: f32 = 0.1;
-    let r = 155;
-    let k = 4;
-    let max_match_cost = 0;
+    let pruning = true;
+    let n = 500;
+    let e: f32 = 0.3;
+    let r = 31415;
+    let k = 6;
+    let max_match_cost = 1;
 
-    let h = CSH {
+    let h = SH {
         match_config: MatchConfig::new(k, max_match_cost),
         pruning,
-        use_gap_cost: false,
-        c: PhantomData::<BruteForceContours>::default(),
     };
 
     let (ref a, ref b, alphabet, _stats) = setup_with_seed(n, e, r);
+    let a = &a[186..219];
+    let b = &b[186..219];
 
     println!("{}\n{}", to_string(a), to_string(b));
     let mut h = h.build(&a, &b, &alphabet);
@@ -25,11 +25,12 @@ fn main() {
     let (distance, _path): (u32, Vec<Pos>) = distance_and_path.unwrap_or_default();
     let dist = bio::alignment::distance::simd::levenshtein(&a, &b);
     assert_eq!(distance, dist);
-    // h.display(
-    //     Pos::from_length(a, b),
-    //     Some(distance),
-    //     Some(astar.explored_states),
-    //     Some(astar.expanded_states),
-    //     Some(path),
-    // );
+    h.display(
+        Pos::from_lengths(a, b),
+        Some(distance),
+        Some(_astar.explored_states),
+        Some(_astar.expanded_states),
+        Some(_path),
+        None,
+    );
 }
