06ae7c716d8982eaec3903147cdf7e99f0effcd5
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-07-27 20:27:32 +0200

    plots.py => figures.py, and remove _N1e7 suffixes

--- evals/plots.py
+++ evals/plots.py
@@ -6,7 +6,7 @@
 from header import *
 
 # Tool comparison / scaling with n.
-df = read_benchmarks("table/tools_N1e7.tsv")
+df = read_benchmarks("table/tools.tsv")
 df = df[df.exit_status == "ok"]
 for e in pd.unique(df.e):
     df_n = df[df.e == e]
@@ -24,21 +24,21 @@ for e in pd.unique(df.e):
 
 # Pruning comparison
 plot_scaling(
-    read_benchmarks("table/scaling_n_N1e7.tsv"),
+    read_benchmarks("table/scaling_n.tsv"),
     x="n",
     y="s_per_pair",
     split=["alg", "r"],
     filename="scaling_n",
     ylog=True,
     xlog=True,
-    cone="cp-csh",
+    cone="csh",
     cone_x=100,
     trend_line="poly",
 )
 
 # Scaling with error rate
 plot_scaling(
-    read_benchmarks("table/scaling_e_N1e6.tsv"),
+    read_benchmarks("table/scaling_e.tsv"),
     x="e_pct",
     y="s_per_pair",
     split=["alg", "r"],
