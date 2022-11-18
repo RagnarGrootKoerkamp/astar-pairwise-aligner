1cf4e2bf5eedf8363ae9765dde3a6e963820ebc2
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-11-18 23:04:40 +0100

    TripleAccel: add align methods

--- src/aligners/triple_accel.rs
+++ src/aligners/triple_accel.rs
@@ -1,9 +1,11 @@
+use triple_accel::levenshtein::EditCosts;
+
 use crate::{
     cost_model::{CostModel, UnitCost},
     prelude::Cost,
 };
 
-use super::{Aligner, Seq};
+use super::{cigar::Cigar, Aligner, Seq};
 
 /// NW aligner for unit costs (Levenshtein distance) only, using library functions.
 #[derive(Debug)]
@@ -12,7 +14,6 @@ pub struct TripleAccel<C: CostModel> {
     pub cost_model: C,
 }
 
-/// TripleAccel only implements `cost()`.
 impl Aligner for TripleAccel<UnitCost> {
     type CostModel = UnitCost;
 
@@ -26,5 +27,46 @@ impl Aligner for TripleAccel<UnitCost> {
         } else {
             triple_accel::levenshtein(a, b)
         }
+    }
+
+    fn align(&mut self, a: Seq, b: Seq) -> (Cost, Cigar) {
+        let (cost, edits) = if self.exp_search {
+            triple_accel::levenshtein::levenshtein_exp_with_opts(
+                a,
+                b,
+                true,
+                EditCosts::new(1, 0, 1, None),
+            )
+        } else {
+            triple_accel::levenshtein::levenshtein_simd_k_with_opts(
+                a,
+                b,
+                u32::MAX,
+                true,
+                EditCosts::new(1, 0, 1, None),
+            )
+            .unwrap()
+        };
+        (cost, Cigar::from_edits(&edits.unwrap()))
+    }
+
+    fn cost_for_bounded_dist(&mut self, a: Seq, b: Seq, f_max: Option<Cost>) -> Option<Cost> {
+        triple_accel::levenshtein::levenshtein_simd_k(a, b, f_max.unwrap())
+    }
+
+    fn align_for_bounded_dist(
+        &mut self,
+        a: Seq,
+        b: Seq,
+        f_max: Option<Cost>,
+    ) -> Option<(Cost, Cigar)> {
+        triple_accel::levenshtein::levenshtein_simd_k_with_opts(
+            a,
+            b,
+            f_max.unwrap(),
+            true,
+            EditCosts::new(1, 0, 1, None),
+        )
+        .map(|(cost, edits)| (cost, Cigar::from_edits(&edits.unwrap())))
     }
 }
