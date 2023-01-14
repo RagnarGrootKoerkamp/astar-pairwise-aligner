5a91b7b940d4232c05ba1b6f7d85f9ab3dd79693
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-01-14 05:42:50 +0100

    Move cli/{input,heuristic_params} into a higher level

--- src/align.rs
+++ src/align.rs
@@ -1,11 +1,85 @@
 use pa_types::{Cigar, Cost, Seq};
 use serde::{Deserialize, Serialize};
 
-use crate::cli::heuristic_params::HeuristicRunner;
-use crate::heuristic::Heuristic;
 use crate::stats::AstarStats;
 use crate::visualizer::*;
-use crate::{astar::astar, astar_dt::astar_dt, cli::heuristic_params::HeuristicArgs};
+use crate::{
+    alignment_graph::*, astar, astar_dt, contour::*, heuristic::*, matches::*, prelude::*,
+};
+use clap::{Parser, ValueEnum};
+use std::marker::PhantomData;
+
+/// The type of the heuristic. Defaults to SH.
+#[derive(Debug, PartialEq, Eq, Default, Clone, Copy, ValueEnum, Serialize, Deserialize)]
+pub enum HeuristicType {
+    None,
+    Zero,
+    Gap,
+    #[default]
+    SH,
+    CSH,
+}
+
+fn default_match_cost() -> MatchCost {
+    2
+}
+fn default_seed_length() -> I {
+    15
+}
+
+/// Heuristic arguments.
+#[derive(Parser, Debug, Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
+#[clap(next_help_heading = "Heuristic")]
+#[serde(deny_unknown_fields)]
+pub struct HeuristicArgs {
+    #[clap(short = 'H', long, default_value_t, value_enum, display_order = 10)]
+    #[serde(rename = "type")]
+    pub heuristic: HeuristicType,
+
+    /// Seed potential
+    ///
+    /// 2 for inexact matches.
+    #[clap(short = 'r', default_value_t = 2, value_name = "r", display_order = 10)]
+    #[serde(default = "default_match_cost")]
+    pub r: MatchCost,
+
+    /// Seed length
+    #[clap(short, value_name = "k", display_order = 10, default_value_t = 15)]
+    #[serde(default = "default_seed_length")]
+    pub k: I,
+
+    /// Minimal seed length
+    #[clap(long, hide_short_help = true)]
+    #[serde(default)]
+    pub kmin: Option<I>,
+
+    /// Maximal seed length
+    #[clap(long, hide_short_help = true)]
+    #[serde(default)]
+    pub kmax: Option<I>,
+
+    /// The maximal number of matches per seed
+    #[clap(long, hide_short_help = true)]
+    #[serde(default)]
+    pub max_matches: Option<usize>,
+
+    /// Disable pruning
+    #[clap(long, hide_short_help = true)]
+    #[serde(default)]
+    pub no_prune: bool,
+
+    /// Skip pruning every Nth match. 0 to disable
+    ///
+    /// This is only used for CSH, not for SH.
+    #[clap(long, hide_short_help = true, default_value_t = 0)]
+    #[serde(default)]
+    pub skip_prune: usize,
+
+    /// Use gap-cost for CSH.
+    #[clap(long, hide_short_help = true)]
+    #[serde(default)]
+    pub gap_cost: bool,
+}
 
 /// The main entrypoint for running A* with some parameters.
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
@@ -17,6 +91,7 @@ pub struct AstarPaParams<V: Visualizer> {
     pub visualizer: V,
 }
 
+/// Alternative configuration using a typed `Heuristic` instance instead of a fixed config.
 #[derive(Debug)]
 pub struct AstarPa<V: Visualizer, H: Heuristic> {
     pub dt: bool,
@@ -57,6 +132,88 @@ impl<V: Visualizer, H: Heuristic> AstarPa<V, H> {
             astar_dt(a, b, &self.h, &self.v)
         } else {
             astar(a, b, &self.h, &self.v)
+        }
+    }
+}
+
+/// A summary string for the visualizer.
+/// Only includes parameters that change the type of algorithm, not numerical values.
+impl ToString for HeuristicArgs {
+    fn to_string(&self) -> String {
+        match self.heuristic {
+            HeuristicType::None => "".into(),
+            HeuristicType::Zero => "Zero".into(),
+            HeuristicType::Gap => "Gap-cost to end".into(),
+            HeuristicType::SH => {
+                let mut s = format!("Seed Heuristic (r={}, k={})", self.r, self.k);
+                if self.no_prune {
+                    s += " (no pruning)"
+                } else {
+                    s += " + Pruning"
+                }
+                s
+            }
+            HeuristicType::CSH => {
+                let mut s = format!("Chaining Seed Heuristic (r={}, k={})", self.r, self.k);
+                if self.no_prune {
+                    s += " (no pruning)"
+                } else {
+                    s += " + Pruning"
+                }
+                if self.gap_cost {
+                    s += " + Gap Cost"
+                }
+                s
+            }
+        }
+    }
+}
+
+pub trait HeuristicRunner {
+    type R;
+    fn call<H: Heuristic>(&self, h: H) -> Self::R;
+}
+
+impl HeuristicArgs {
+    pub fn match_config(&self, window_filter: bool) -> MatchConfig {
+        let r = self.r;
+        let k = self.k;
+        MatchConfig {
+            length: if let Some(max) = self.max_matches {
+                LengthConfig::Max(crate::matches::MaxMatches {
+                    max_matches: max,
+                    k_min: self.kmin.unwrap_or(k),
+                    k_max: self.kmax.unwrap_or(k),
+                })
+            } else {
+                LengthConfig::Fixed(k)
+            },
+            max_match_cost: r - 1,
+            window_filter,
+        }
+    }
+
+    pub fn run_on_heuristic<F: HeuristicRunner>(&self, f: F) -> F::R {
+        match self.heuristic {
+            HeuristicType::None => f.call(NoCost),
+            HeuristicType::Zero => f.call(ZeroCost),
+            HeuristicType::Gap => f.call(GapCost),
+            HeuristicType::CSH => f.call(CSH {
+                match_config: self.match_config(self.gap_cost),
+                pruning: Pruning {
+                    enabled: !self.no_prune,
+                    skip_prune: self.skip_prune,
+                },
+                use_gap_cost: self.gap_cost,
+                c: PhantomData::<HintContours<BruteForceContour>>,
+            }),
+            HeuristicType::SH => f.call(SH {
+                match_config: self.match_config(false),
+                pruning: Pruning {
+                    enabled: !self.no_prune,
+                    skip_prune: self.skip_prune,
+                },
+            }),
         }
     }
 }
--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,17 @@
-pub mod heuristic_params;
-pub mod input;
-
-use std::{path::PathBuf, time::Duration};
-
-use crate::cli::input::Input;
+use crate::{prelude::Seq, HeuristicArgs};
+use bio::io::fasta;
 use clap::{value_parser, Parser};
-use heuristic_params::HeuristicArgs;
+use itertools::Itertools;
+use rand::{Rng, SeedableRng};
+use rand_chacha::ChaCha8Rng;
 use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    ops::ControlFlow,
+    path::PathBuf,
+    time::Duration,
+};
 
 #[derive(Parser, Serialize, Deserialize)]
 #[clap(author, about)]
@@ -36,4 +41,96 @@ pub struct Cli {
     /// Stop aligning new pairs after this timeout.
     #[arg(long, value_parser = parse_duration::parse, hide_short_help = true)]
     pub timeout: Option<Duration>,
+}
+
+#[derive(Parser, Serialize, Deserialize)]
+#[clap(next_help_heading = "Input")]
+pub struct Input {
+    /// The .seq, .txt, or Fasta file with sequence pairs to align.
+    #[clap(short, long, value_parser = value_parser!(PathBuf), display_order = 1)]
+    input: Option<PathBuf>,
+
+    /// Options to generate an input pair.
+    #[clap(flatten)]
+    generate: pa_generate::DatasetGenerator,
+}
+
+impl Input {
+    /// Call the given function for each pair in the input.
+    pub fn process_input_pairs(&self, mut run_pair: impl FnMut(Seq, Seq) -> ControlFlow<()>) {
+        let mut run_cropped_pair = |mut a: Seq, mut b: Seq| -> ControlFlow<()> {
+            // Shrink if needed.
+            let n = self.generate.settings.length;
+            if n > 0 {
+                if a.len() > n {
+                    a = &a[..n];
+                }
+                if b.len() > n {
+                    b = &b[..n];
+                }
+            }
+            run_pair(a, b)
+        };
+
+        if let Some(input) = &self.input {
+            let files = if input.is_file() {
+                vec![input.clone()]
+            } else {
+                input
+                    .read_dir()
+                    .unwrap()
+                    .map(|x| x.unwrap().path())
+                    .collect_vec()
+            };
+
+            'outer: for f in files {
+                match f.extension().expect("Unknown file extension") {
+                    ext if ext == "seq" || ext == "txt" => {
+                        let f = std::fs::File::open(&f).unwrap();
+                        let f = BufReader::new(f);
+                        for (mut a, mut b) in f.lines().map(|l| l.unwrap().into_bytes()).tuples() {
+                            if ext == "seq" {
+                                assert_eq!(a.remove(0), '>' as u8);
+                                assert_eq!(b.remove(0), '<' as u8);
+                            }
+                            if let ControlFlow::Break(()) = run_cropped_pair(&a, &b) {
+                                break 'outer;
+                            }
+                        }
+                    }
+                    ext if ext == "fna" || ext == "fa" || ext == "fasta" => {
+                        for (a, b) in fasta::Reader::new(BufReader::new(File::open(&f).unwrap()))
+                            .records()
+                            .tuples()
+                        {
+                            if let ControlFlow::Break(()) =
+                                run_cropped_pair(a.unwrap().seq(), b.unwrap().seq())
+                            {
+                                break 'outer;
+                            }
+                        }
+                    }
+                    ext => {
+                        unreachable!(
+                            "Unknown file extension {ext:?}. Must be in {{seq,txt,fna,fa,fasta}}."
+                        )
+                    }
+                };
+            }
+        } else {
+            // Generate random input.
+            let seed = self.generate.seed.unwrap_or_else(|| {
+                let seed = ChaCha8Rng::from_entropy().gen_range(0..u64::MAX);
+                eprintln!("Seed: {seed}");
+                seed
+            });
+            let ref mut rng = ChaCha8Rng::seed_from_u64(seed);
+            for _ in 0..self.generate.cnt.unwrap() {
+                let (a, b) = self.generate.settings.generate(rng);
+                if let ControlFlow::Break(()) = run_pair(&a, &b) {
+                    break;
+                }
+            }
+        }
+    }
 }
