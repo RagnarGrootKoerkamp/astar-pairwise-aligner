cf2f19c250552e5a3e18f13730248926c791c37d
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-01-18 12:08:47 +0100

    Add AffineAligner to pa-affine-types

--- pa-affine-types/src/lib.rs
+++ pa-affine-types/src/lib.rs
@@ -1,4 +1,4 @@
-use pa_types::I;
+use pa_types::{Cost, Seq, I};
 
 pub mod cigar;
 pub mod cost_model;
@@ -21,4 +21,12 @@ impl State {
     pub fn new(i: I, j: I, layer: Layer) -> Self {
         Self { i, j, layer }
     }
+}
+
+/// Generic pairwise global alignment interface.
+pub trait AffineAligner {
+    /// An alignment of sequences `a` and `b`.
+    /// The returned cost is the *non-negative* cost of the alignment.
+    /// Returns a trace when specified on construction.
+    fn align(&mut self, a: Seq, b: Seq) -> (Cost, Option<AffineCigar>);
 }
