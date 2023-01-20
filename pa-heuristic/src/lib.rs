5005d1b0a06e74615ec603dc61f5bf7b3c04e42e
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-01-20 13:04:00 +0100

    Disable clap default features

--- pa-heuristic/src/lib.rs
+++ pa-heuristic/src/lib.rs
@@ -1,6 +1,5 @@
 #![feature(is_sorted, associated_type_defaults, drain_filter)]
 
-#[cfg(feature = "cli")]
 mod cli;
 mod config;
 mod contour;
@@ -8,7 +7,6 @@ pub mod heuristic;
 pub mod matches;
 mod split_vec;
 
-#[cfg(feature = "cli")]
 pub use cli::*;
 pub use heuristic::*;
 pub use matches::MatchConfig;
