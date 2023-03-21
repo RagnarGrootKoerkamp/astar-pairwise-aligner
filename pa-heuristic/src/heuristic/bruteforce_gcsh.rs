80aeb453c0d3046fe2e901b81ff3b6819d26b280
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-03-22 00:59:38 +0100

    Bruteforce contours: Do check for consistency with gapcost, to be consistent with GCSH
    (even though in itself this isn't needed for bruteforce contours)

--- pa-heuristic/src/heuristic/bruteforce_gcsh.rs
+++ pa-heuristic/src/heuristic/bruteforce_gcsh.rs
@@ -76,7 +76,12 @@ where
             distance_function: Distance::build(&params.distance_function, a, b),
             target: Pos::target(a, b),
             seeds,
-            matches: MatchPruner::new(params.pruning, false, matches),
+            matches: MatchPruner::new(
+                params.pruning,
+                // Ensure consistency with GCSH.
+                params.distance_function.name() == "Gap",
+                matches,
+            ),
             h_at_matches: Default::default(),
             stats: Default::default(),
         };
