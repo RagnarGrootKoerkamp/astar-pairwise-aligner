f576b9817ed39e00abcb04d6e0a7746c08fa7979
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-08-31 02:34:21 +0200

    [tests] Add flag to make tests deterministic

--- pa-test/src/lib.rs
+++ pa-test/src/lib.rs
@@ -18,20 +18,26 @@ fn test_sequences() -> Vec<(Seq<'static>, Seq<'static>)> {
     ]
 }
 
+const FIXED: bool = false;
+
 pub fn gen_seqs() -> impl Iterator<Item = ((Sequence, Sequence), (usize, f32, ErrorModel, u64))> {
     let rng = &mut thread_rng();
-    let ns = [
+    let mut ns = vec![
         0usize, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50,
         60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240,
         250, 260, 270, 280, 290, 300, 500,
     ];
-    let es = [
+    let mut es = vec![
         0.0f32, 0.01, 0.02, 0.03, 0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 1.0,
     ];
 
     // Pick a random subset of the above for 4x speedup. CI runs often enough to get good coverage.
-    let ns = ns.into_iter().choose_multiple(rng, ns.len() / 4);
-    let es = es.into_iter().choose_multiple(rng, es.len() / 4);
+    if !FIXED {
+        let nl = ns.len();
+        ns = ns.into_iter().choose_multiple(rng, nl / 4);
+        let el = es.len();
+        es = es.into_iter().choose_multiple(rng, el / 4);
+    }
 
     let models = [
         ErrorModel::Uniform,
@@ -40,7 +46,11 @@ pub fn gen_seqs() -> impl Iterator<Item = ((Sequence, Sequence), (usize, f32, Er
         ErrorModel::SymmetricRepeat,
     ];
     // Run each test on a new random seed for increased coverage over time.
-    let seeds = [rng.gen_range(0..u64::MAX)];
+    let seeds = if FIXED {
+        [31415]
+    } else {
+        [rng.gen_range(0..u64::MAX)]
+    };
     ns.into_iter()
         .cartesian_product(es)
         .cartesian_product(models)
