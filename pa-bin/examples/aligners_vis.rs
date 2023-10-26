5ce5e28aa552485e52feae969ff1f6e248292e39
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-10-26 16:17:06 +0200

    update aligners_vis visualization examples

--- pa-bin/examples/aligners_vis.rs
+++ pa-bin/examples/aligners_vis.rs
@@ -1,3 +1,4 @@
+use astarpa::AstarPa;
 use pa_affine_types::AffineCost;
 use pa_base_algos::{
     dt::{DiagonalTransition, GapCostHeuristic},
@@ -5,7 +6,7 @@ use pa_base_algos::{
     Domain,
 };
 use pa_generate::uniform_fixed;
-use pa_heuristic::{GapCost, MatchConfig, NoCost, Pruning, CSH, SH};
+use pa_heuristic::{GapCost, MatchConfig, NoCost, Pruning, CSH, GCSH, SH};
 use pa_types::seq_to_string;
 use pa_vis::visualizer::{self, Gradient, When};
 use std::{path::PathBuf, time::Duration};
@@ -18,13 +19,24 @@ fn main() {
 
     let cm = AffineCost::unit();
     let mut config = visualizer::Config::default();
-    config.draw = When::All;
-    config.save = When::Last;
+    config.draw = When::Layers;
+    config.save = When::None;
+    config.save_last = false;
     config.delay = Duration::from_secs_f32(0.0001);
-    config.cell_size = 2;
+    config.cell_size = 0;
     config.style.bg_color = (255, 255, 255, 128);
     config.style.expanded = Gradient::TurboGradient(0.25..0.90);
+    config.style.path = Some((0, 0, 0, 0));
+    config.style.path_width = Some(4);
+    config.layer_drawing = false;
+    config.style.draw_dt = false;
+    config.style.draw_f = false;
+    config.style.draw_labels = false;
+    config.transparent_bmp = true;
     config.draw_old_on_top = true;
+    config.clear_after_meeting_point = false;
+    config.paused = true;
+
     let mut vis = |name: &str| {
         config.filepath = PathBuf::from("imgs/").join(name);
         config.clone()
@@ -35,7 +47,16 @@ fn main() {
         pruning: Pruning::disabled(),
     };
     let csh = CSH::new(MatchConfig::exact(4), Pruning::disabled());
+    let gcsh = GCSH::new(MatchConfig::exact(4), Pruning::disabled());
 
+    {
+        let aligner = AstarPa {
+            dt: false,
+            h: NoCost,
+            v: vis("dijkstra"),
+        };
+        aligner.align(a, b);
+    }
     {
         let nw = NW {
             cm: cm.clone(),
@@ -136,47 +157,87 @@ fn main() {
         dt.align(a, b);
     }
 
-    {
-        let mut dt = DiagonalTransition::new(
-            cm.clone(),
-            GapCostHeuristic::Enable,
-            NoCost,
-            false,
-            vis("dt_gapcost"),
-        );
-        dt.align(a, b);
-    }
+    // {
+    //     let mut dt = DiagonalTransition::new(
+    //         cm.clone(),
+    //         GapCostHeuristic::Enable,
+    //         NoCost,
+    //         false,
+    //         vis("dt_gapcost"),
+    //     );
+    //     dt.align(a, b);
+    // }
 
-    {
-        let mut dt = DiagonalTransition::new(
-            cm.clone(),
-            GapCostHeuristic::Disable,
-            GapCost,
-            false,
-            vis("dt_gapcost_h"),
-        );
-        dt.align(a, b);
-    }
+    // {
+    //     let mut dt = DiagonalTransition::new(
+    //         cm.clone(),
+    //         GapCostHeuristic::Disable,
+    //         GapCost,
+    //         false,
+    //         vis("dt_gapcost_h"),
+    //     );
+    //     dt.align(a, b);
+    // }
 
-    {
-        let mut dt = DiagonalTransition::new(
-            cm.clone(),
-            GapCostHeuristic::Disable,
-            sh,
-            false,
-            vis("dt_sh"),
-        );
-        dt.align(a, b);
-    }
+    // {
+    //     let mut dt = DiagonalTransition::new(
+    //         cm.clone(),
+    //         GapCostHeuristic::Disable,
+    //         sh,
+    //         false,
+    //         vis("dt_sh"),
+    //     );
+    //     dt.align(a, b);
+    // }
 
+    // {
+    //     let mut dt = DiagonalTransition::new(
+    //         cm.clone(),
+    //         GapCostHeuristic::Disable,
+    //         csh,
+    //         false,
+    //         vis("dt_csh"),
+    //     );
+    //     dt.align(a, b);
+    // }
     {
-        let mut dt = DiagonalTransition::new(
-            cm.clone(),
-            GapCostHeuristic::Disable,
-            csh,
-            false,
-            vis("dt_csh"),
-        );
-        dt.align(a, b);
+        let aligner = AstarPa {
+            dt: false,
+            h: sh,
+            v: vis("a*pa-sh"),
+        };
+        aligner.align(a, b);
+    }
+    {
+        let aligner = AstarPa {
+            dt: false,
+            h: csh,
+            v: vis("a*pa-csh"),
+        };
+        aligner.align(a, b);
+    }
+    {
+        let aligner = AstarPa {
+            dt: false,
+            h: gcsh,
+            v: vis("a*pa-gcsh"),
+        };
+        aligner.align(a, b);
+    }
+    {
+        let aligner = AstarPa {
+            dt: true,
+            h: sh,
+            v: vis("a*pa-sh-dt"),
+        };
+        aligner.align(a, b);
+    }
+    {
+        let aligner = AstarPa {
+            dt: true,
+            h: gcsh,
+            v: vis("a*pa-gcsh-dt"),
+        };
+        aligner.align(a, b);
     }
 }
